a49ccabe4ad45362cb198b9b0554d6786ac469cf
simon louvet <simon.louvet.zen@gmail.com>
2019-12-06 18:23:29 +0100

    config by url instead ENV

--- sources/SolidMiddleware/boilerplate/runner/index.js
+++ sources/SolidMiddleware/boilerplate/runner/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const { ServiceBroker } = require('moleculer');
+const {
+  ServiceBroker
+} = require('moleculer');
 const ApiGatewayService = require('moleculer-web');
 const dummyServiceMath = require('@semapps/dummyservicemath');
 const activityPub = require('@semapps/activitypub');
 const os = require('os');
 const hostname = os.hostname();
+const fetch = require('node-fetch');
 
 // let transporter = process.env.TRANSPORTER || "TCP";
 const transporter = null;
@@ -19,42 +22,48 @@ const transporter = null;
 // 	logLevel: "warn"
 // 	//metrics: true
 // });
+const start =  async function() {
+  let response = await fetch("https://assemblee-virtuelle.gitlab.io/semappsconfig/local.json");
+  let config = await response.json();
+  console.log(config);
 
-const broker = new ServiceBroker({
-  nodeID: process.argv[2] || hostname + '-server',
-  logger: console,
-  transporter: transporter
-});
+  const broker = new ServiceBroker({
+    nodeID: process.argv[2] || hostname + '-server',
+    logger: console,
+    transporter: transporter
+  });
 
-broker.createService(dummyServiceMath);
-broker.createService(activityPub, {
-  settings: {
-    homeUrl: process.env.HOME_URL || 'http://localhost:3000/',
-    sparqlEndpoint: process.env.SPARQL_ENDPOINT
-  }
-});
-broker.createService({
-  mixins: ApiGatewayService,
-  settings: {
-    middleware: true,
-    routes: [activityPub.routes]
-  }
-});
-broker.start();
+  broker.createService(dummyServiceMath);
+  broker.createService(activityPub, {
+    settings: {
+      homeUrl: config.home_url || 'http://localhost:3000/',
+      sparqlEndpoint: config.sparql_endpoint
+    }
+  });
+  broker.createService({
+    mixins: ApiGatewayService,
+    settings: {
+      middleware: true,
+      routes: [activityPub.routes]
+    }
+  });
+  broker.start();
 
-console.log('Server started. nodeID: ', broker.nodeID, ' TRANSPORTER:', transporter, ' PID:', process.pid);
+  console.log('Server started. nodeID: ', broker.nodeID, ' TRANSPORTER:', transporter, ' PID:', process.pid);
 
-setInterval(() => {
-  let payload = {
-    a: Math.random(0, 100),
-    b: Math.random(0, 100)
-  };
-  broker
-    .call('math.add', payload)
-    .then(res => {
-      console.log('brocker call result : ', res);
-    })
-    .catch(err => {
-      throw err;
-    });
-}, 1000);
+  setInterval(() => {
+    let payload = {
+      a: Math.random(0, 100),
+      b: Math.random(0, 100)
+    };
+    broker
+      .call('math.add', payload)
+      .then(res => {
+        console.log('brocker call result : ', res);
+      })
+      .catch(err => {
+        throw err;
+      });
+  }, 1000);
+};
+start();
