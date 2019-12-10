31b9fceed04e214c011b6ae351d3aed0f4b972bc
srosset81 <srosset81@gmail.com>
2019-12-10 19:22:18 +0100

    Initial frontend

---
+++ sources/frontend/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+    const [ title, setTitle ] = useState("");
+    const [ content, setContent ] = useState("");
+
+    const sendNote = async () => {
+        const note = {
+            '@context': "https://www.w3.org/ns/activitystreams",
+            type: "Note",
+            name: title,
+            content: content,
+            'published': "2019-05-28T12:12:12Z"
+        };
+
+        const response = await fetch('http://localhost:3000/activitypub/outbox', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(note)
+        });
+
+        const activity = await response.json();
+
+        alert('Activity created with ID : ' + activity.id);
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p className="App-logo">
+                    SemApps Playground
+                </p>
+            </header>
+            <div className="App-form">
+                <input value={title} onChange={e => setTitle(e.target.value)}/>
+                <textarea rows="7" value={content} onChange={e => setContent(e.target.value)}/>
+                <button onClick={sendNote}>Envoyer le message</button>
+            </div>
+        </div>
+    );
+};
+
+export default App;
