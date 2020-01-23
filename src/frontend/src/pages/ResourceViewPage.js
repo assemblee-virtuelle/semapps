cea28a9b27039b6a7b98d564512724aceb9e3a33
srosset81 <srosset81@users.noreply.github.com>
2020-01-23 12:17:21 +0000

    Automatic prettier

--- src/frontend/src/pages/ResourceViewPage.js
+++ src/frontend/src/pages/ResourceViewPage.js
@@ -12,51 +12,53 @@ const ResourceViewPage = ({ resourceId }) => {
   // TODO improve this code, that we must do since sometimes the LDP server
   // returns the webpage as an object with the value in the @id property
   let webPage;
-  if( resource ) webPage = resource.webPage || resource['pair:webPage'];
-  if( typeof webPage === 'object' ) webPage = webPage['@id'];
+  if (resource) webPage = resource.webPage || resource['pair:webPage'];
+  if (typeof webPage === 'object') webPage = webPage['@id'];
 
   return (
-      <Page>
-        {resource && (
-          <>
-            <h2>{resource.label || resource['pair:label']}</h2>
-            <ul className="list-group">
-              <div className="list-group-item">
-                <div>
-                  <strong>Titre</strong>
-                </div>
-                <div>{resource.label || resource['pair:label']}</div>
+    <Page>
+      {resource && (
+        <>
+          <h2>{resource.label || resource['pair:label']}</h2>
+          <ul className="list-group">
+            <div className="list-group-item">
+              <div>
+                <strong>Titre</strong>
               </div>
-              <div className="list-group-item">
-                <div>
-                  <strong>Description</strong>
-                </div>
-                <div>{nl2br(resource.description || resource['pair:description'])}</div>
+              <div>{resource.label || resource['pair:label']}</div>
+            </div>
+            <div className="list-group-item">
+              <div>
+                <strong>Description</strong>
               </div>
-              <div className="list-group-item">
-                <div>
-                  <strong>Site web</strong>
-                </div>
-                <div>
-                  <a href={webPage} target="_blank" rel="noopener noreferrer">{webPage}</a>
-                </div>
+              <div>{nl2br(resource.description || resource['pair:description'])}</div>
+            </div>
+            <div className="list-group-item">
+              <div>
+                <strong>Site web</strong>
               </div>
-            </ul>
-            <br />
-            <Link to={`/resources/${resourceId}/edit`}>
-              <button type="submit" className="btn btn-warning">
-                Modifier
-              </button>
-            </Link>
-            &nbsp; &nbsp;
-            <Link to={`/resources/${resourceId}/delete`}>
-              <button type="submit" className="btn btn-danger">
-                Effacer
-              </button>
-            </Link>
-          </>
-        )}
-      </Page>
+              <div>
+                <a href={webPage} target="_blank" rel="noopener noreferrer">
+                  {webPage}
+                </a>
+              </div>
+            </div>
+          </ul>
+          <br />
+          <Link to={`/resources/${resourceId}/edit`}>
+            <button type="submit" className="btn btn-warning">
+              Modifier
+            </button>
+          </Link>
+          &nbsp; &nbsp;
+          <Link to={`/resources/${resourceId}/delete`}>
+            <button type="submit" className="btn btn-danger">
+              Effacer
+            </button>
+          </Link>
+        </>
+      )}
+    </Page>
   );
 };
 
