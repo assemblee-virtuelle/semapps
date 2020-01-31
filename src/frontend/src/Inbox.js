c752f5180f6487847246f1827c259057b8cef848
srosset81 <srosset81@gmail.com>
2020-01-31 19:45:30 +0100

    Split Inbox component

--- src/frontend/src/Inbox.js
+++ src/frontend/src/Inbox.js
@@ -3,34 +3,36 @@ import { Link } from '@reach/router';
 import useQuery from './api/useQuery';
 import { getResourceId, nl2br } from './utils';
 
+const Activity = ({ activityUri }) => {
+  const { data } = useQuery(activityUri);
+  if (data.type === 'Create' && data.object) {
+    const username = getResourceId(data.actor, 'users');
+    const published = new Date(Date.parse(data.published));
+    return (
+      <div className="list-group-item">
+        <div className="d-flex w-100 justify-content-between">
+          <h5 className="mb-1">{data.object.name}</h5>
+          <small>{published.toLocaleString('fr-FR')}</small>
+        </div>
+        <p className="mb-1">{nl2br(data.object.content)}</p>
+        <small>
+          Envoyé par <Link to={`/resources/users/${username}`}>{username}</Link>
+        </small>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
+
 const Inbox = ({ inboxUri }) => {
   const { data } = useQuery(inboxUri);
   return (
     <>
       <h2 className="mt-3">Messages reçus</h2>
       <ul className="list-group">
-        {data &&
-          data.orderedItems.map((item, i) => {
-            if (item.type === 'Create' && item.object) {
-              const username = getResourceId(item.actor, 'users');
-              const published = new Date(Date.parse(item.published));
-              return (
-                <div className="list-group-item" key={i}>
-                  <div className="d-flex w-100 justify-content-between">
-                    <h5 className="mb-1">{item.object.name}</h5>
-                    <small>{published.toLocaleString('fr-FR')}</small>
-                  </div>
-                  <p className="mb-1">{nl2br(item.object.content)}</p>
-                  <small>
-                    Envoyé par <Link to={`/resources/users/${username}`}>{username}</Link>
-                  </small>
-                </div>
-              );
-            } else {
-              return null;
-            }
-          })}
-        {data && data.totalItems === 0 && <span>Aucun message n'a été reçu pour le moment.</span>}
+        {data && data.map((item, i) => <Activity activityUri={item} key={i} />)}
+        {data && data.length === 0 && <span>Aucun message n'a été reçu pour le moment.</span>}
       </ul>
     </>
   );
