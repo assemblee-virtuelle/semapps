346f6f9d62e690974162df5c51fd05c2566d9be7
bb <info@parlepeuple.fr>
2020-02-05 20:25:14 +0200

    TODO : set X-SemappsUser to 'system' or to the URI of the logged in user

--- src/middleware/packages/triplestore/index.js
+++ src/middleware/packages/triplestore/index.js
@@ -85,6 +85,7 @@ const TripleStoreService = {
       return results.length;
     },
     async query({ params }) {
+      // TODO : set X-SemappsUser to 'system' or to the URI of the logged in user
       const headers = {
         'Content-Type': 'application/sparql-query',
         'X-SemappsUser': 'URIofUser',
