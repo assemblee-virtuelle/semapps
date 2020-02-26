3ec79c25f924f6f1efa53dd86ef42cf71dfcf6a1
srosset81 <srosset81@gmail.com>
2020-02-26 19:19:28 +0100

    Adapt code after merge

--- src/middleware/boilerplates/mailer/index.js
+++ src/middleware/boilerplates/mailer/index.js
@@ -1,15 +1,7 @@
 const { ServiceBroker } = require('moleculer');
 const MongoDbAdapter = require('moleculer-db-adapter-mongo');
 const express = require('express');
-const {
-  ActivityService,
-  OutboxService,
-  InboxService,
-  FollowService,
-  MongoDbCollectionService,
-  ActorService,
-  ObjectService
-} = require('@semapps/activitypub');
+const { ActivityPubService } = require('@semapps/activitypub');
 
 const FormService = require('./services/form');
 const ApiService = require('./services/api');
@@ -18,43 +10,22 @@ const MailerService = require('./services/mailer');
 const ThemeService = require('./services/theme');
 const CONFIG = require('./config');
 
-const context = {
-  '@vocab': 'https://www.w3.org/ns/activitystreams#',
-  pair: 'http://virtual-assembly.org/ontologies/pair#'
-};
-
 const broker = new ServiceBroker();
 
-broker.createService(MongoDbCollectionService, {
-  adapter: new MongoDbAdapter(CONFIG.MONGODB_URL),
-  settings: {
-    context
+broker.createService(ActivityPubService, {
+  baseUri: CONFIG.HOME_URL,
+  context: {
+    '@vocab': 'https://www.w3.org/ns/activitystreams#',
+    foaf: 'http://xmlns.com/foaf/0.1/',
+    pair: 'http://virtual-assembly.org/ontologies/pair#'
+  },
+  storage: {
+    collections: new MongoDbAdapter(CONFIG.MONGODB_URL),
+    activities: new MongoDbAdapter(CONFIG.MONGODB_URL),
+    actors: new MongoDbAdapter(CONFIG.MONGODB_URL),
+    objects: new MongoDbAdapter(CONFIG.MONGODB_URL),
   }
 });
-broker.createService(ActorService, {
-  adapter: new MongoDbAdapter(CONFIG.MONGODB_URL),
-  settings: {
-    containerUri: CONFIG.HOME_URL + 'users/',
-    context
-  }
-});
-broker.createService(ActivityService, {
-  adapter: new MongoDbAdapter(CONFIG.MONGODB_URL),
-  settings: {
-    containerUri: CONFIG.HOME_URL + 'activities/',
-    context
-  }
-});
-broker.createService(ObjectService, {
-  adapter: new MongoDbAdapter(CONFIG.MONGODB_URL),
-  settings: {
-    containerUri: CONFIG.HOME_URL + 'objects/',
-    context
-  }
-});
-broker.createService(FollowService);
-broker.createService(InboxService);
-broker.createService(OutboxService);
 
 broker.createService(ThemeService, {
   adapter: new MongoDbAdapter(CONFIG.MONGODB_URL),
@@ -89,7 +60,7 @@ broker.start().then(() => {
     if (err) {
       console.error(err);
     } else {
-      console.log('Listening on port ' + process.env.NODE_PORT || 3000);
+      console.log('Listening on port ' + ( process.env.NODE_PORT || 3000 ));
     }
   });
 });
