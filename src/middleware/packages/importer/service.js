6cba62bd15aec1157f3bbfb4f224b486c3b2ff7d
srosset81 <srosset81@gmail.com>
2020-03-27 15:59:55 +0100

    Remove userId param

--- src/middleware/packages/importer/service.js
+++ src/middleware/packages/importer/service.js
@@ -1,14 +1,11 @@
-const { MIME_TYPES } = require('@semapps/mime-types');
 const fs = require('fs').promises;
 const path = require('path');
 
 const ImporterService = {
   name: 'importer',
   settings: {
-    // To be set by user
     baseUri: null,
     baseDir: null,
-    usersContainer: null,
     allowedActions: []
   },
   async started() {
@@ -20,13 +17,11 @@ const ImporterService = {
   },
   actions: {
     async import(ctx) {
-      let { action, fileName, userId, ...otherParams } = ctx.params;
+      let { action, fileName, ...otherParams } = ctx.params;
 
       if (!action || !fileName || !this.settings.allowedActions.includes(action)) {
         throw new MoleculerError('Bad request', 400, 'BAD_REQUEST');
       }
-
-      if (userId && !userId.startsWith('http')) userId = this.settings.usersContainer + userId;
 
       const file = await fs.readFile(path.resolve(this.settings.baseDir, fileName));
       const json = JSON.parse(file.toString());
@@ -34,7 +29,7 @@ const ImporterService = {
       console.log(`Importing ${json.length} items...`);
 
       for (let data of json) {
-        await this.actions[action]({ data, userId, ...otherParams });
+        await this.actions[action]({ data, ...otherParams });
       }
 
       console.log('Import finished !');
