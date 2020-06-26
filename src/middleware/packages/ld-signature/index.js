0eb3002a4b9fca162700630c32290b19e061cacf
srosset81 <srosset81@gmail.com>
2020-06-26 12:52:30 +0200

    Add LdSignatureService and generate a key pair on every actor creation

---
+++ src/middleware/packages/ld-signature/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  LdSignatureService: require('./service')
+};
---
+++ src/middleware/packages/ld-signature/service.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const { generateKeyPair } = require('crypto');
+
+const getSlugFromUri = str => str.replace(/\/$/, '').replace(/.*\//, '');
+
+const LdSignatureService = {
+  name: 'ld-signature',
+  settings: {
+    actorsKeyPairsDir: null,
+  },
+  actions: {
+    async generateActorKeyPair(ctx) {
+      return new Promise((resolve, reject) => {
+        const { actorUri } = ctx.params;
+        const actorId = getSlugFromUri(actorUri);
+
+        generateKeyPair('rsa', {
+          modulusLength: 4096,
+          publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem'
+          },
+          privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem'
+          }
+        }, (err, publicKey, privateKey) => {
+          if (!err) {
+            fs.writeFile(path.join(this.settings.actorsKeyPairsDir, actorId + '.key'), privateKey, err => reject(err));
+            fs.writeFile(path.join(this.settings.actorsKeyPairsDir, actorId + '.key.pub'), publicKey, err => reject(err));
+            resolve(publicKey);
+          } else {
+            reject(err);
+          }
+        })
+      });
+    }
+  }
+};
+
+module.exports = LdSignatureService;
