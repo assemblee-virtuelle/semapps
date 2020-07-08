67a7c5a75a7fa55992479ff458e8aa018fd89343
srosset81 <srosset81@gmail.com>
2020-07-08 10:07:27 +0200

    Use custom Redux store

--- src/frontend/dms/src/App.js
+++ src/frontend/dms/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createHashHistory } from 'history';
 import { Admin, Resource } from 'react-admin';
 import { dataProvider, authProvider, httpClient } from '@semapps/react-admin';
 import LogoutButton from './auth/LogoutButton';
@@ -8,39 +10,51 @@ import { PersonList, PersonIcon } from './resources/persons';
 import { ConceptList, ConceptIcon } from './resources/concepts';
 import resources from './config/resources';
 import ontologies from './config/ontologies';
+import createStore from './createStore';
+
+const history = createHashHistory();
 
 function App() {
   return (
-    <Admin
-      dataProvider={dataProvider({
-        sparqlEndpoint: process.env.REACT_APP_MIDDLEWARE_URL + 'sparql',
-        httpClient,
-        resources,
-        ontologies
+    <Provider
+      store={createStore({
+        authProvider,
+        dataProvider,
+        history,
       })}
-      authProvider={authProvider(process.env.REACT_APP_MIDDLEWARE_URL)}
-      logoutButton={LogoutButton}
     >
-      <Resource
-        name="Project"
-        list={ProjectList}
-        edit={ProjectEdit}
-        create={ProjectCreate}
-        icon={ProjectIcon}
-        options={{ label: 'Projets' }}
-      />
-      <Resource
-        name="Organization"
-        list={OrganizationList}
-        edit={OrganizationEdit}
-        create={OrganizationCreate}
-        icon={OrganizationIcon}
-        options={{ label: 'Organisations' }}
-      />
-      <Resource name="Person" list={PersonList} icon={PersonIcon} options={{ label: 'Contributeurs' }} />
-      <Resource name="Concept" list={ConceptList} icon={ConceptIcon} options={{ label: 'Concepts' }} />
-      <Resource name="Agent" />
-    </Admin>
+      <Admin
+        dataProvider={dataProvider({
+          sparqlEndpoint: process.env.REACT_APP_MIDDLEWARE_URL + 'sparql',
+          httpClient,
+          resources,
+          ontologies
+        })}
+        authProvider={authProvider(process.env.REACT_APP_MIDDLEWARE_URL)}
+        history={history}
+        logoutButton={LogoutButton}
+      >
+        <Resource
+          name="Project"
+          list={ProjectList}
+          edit={ProjectEdit}
+          create={ProjectCreate}
+          icon={ProjectIcon}
+          options={{ label: 'Projets' }}
+        />
+        <Resource
+          name="Organization"
+          list={OrganizationList}
+          edit={OrganizationEdit}
+          create={OrganizationCreate}
+          icon={OrganizationIcon}
+          options={{ label: 'Organisations' }}
+        />
+        <Resource name="Person" list={PersonList} icon={PersonIcon} options={{ label: 'Contributeurs' }} />
+        <Resource name="Concept" list={ConceptList} icon={ConceptIcon} options={{ label: 'Concepts' }} />
+        <Resource name="Agent" />
+      </Admin>
+    </Provider>
   );
 }
 
