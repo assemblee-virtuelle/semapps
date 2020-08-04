f50f25d75a60b041bc8a221d36cf07c8b407fd66
srosset81 <srosset81@gmail.com>
2020-08-04 10:53:06 +0200

    Use AV layout

--- src/frontend/dms/src/components/Layout.js
+++ src/frontend/dms/src/components/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Notification } from 'react-admin';
-import { Container, Box, ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { Container, Box, ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 const theme = createMuiTheme({
   palette: {
     grey: { main: '#e0e0e0' },
-    primary: { main: '#61d2fe' }
+    primary: { main: '#28ccfb' }
   },
   typography: {
     details: {
@@ -36,26 +36,41 @@ const theme = createMuiTheme({
   }
 });
 
-const Layout = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <Box bgcolor="grey.main">
-      <Container maxWidth="lg">
-        <Box pb={3} pt={3}>
+const useStyles = makeStyles(() => ({
+  header: {
+    position: 'relative',
+    height: 65,
+    marginBottom: 35
+  },
+  logo: {
+    position: 'absolute',
+    top: -15,
+    height: 110,
+    width: 110
+  }
+}));
+
+const Layout = ({ children }) => {
+  const classes = useStyles();
+  return (
+    <ThemeProvider theme={theme}>
+      <Box bgcolor="primary.main">
+        <Container maxWidth="lg" className={classes.header}>
           <Link to="/Project">
             <img
-              src={process.env.PUBLIC_URL + '/av-chantiers-collaboratifs.png'}
-              style={{ maxHeight: 125, maxWidth: '100%' }}
-              alt="Chantiers collaboratifs"
+              src={process.env.PUBLIC_URL + '/av.png'}
+              alt="Assemblée Virtuelle"
+              className={classes.logo}
             />
           </Link>
-        </Box>
+        </Container>
+      </Box>
+      <Container maxWidth="lg">
+        <Box mb={5}>{children}</Box>
       </Container>
-    </Box>
-    <Container maxWidth="lg">
-      <Box mb={5}>{children}</Box>
-    </Container>
-    <Notification />
-  </ThemeProvider>
-);
+      <Notification />
+    </ThemeProvider>
+  );
+}
 
 export default Layout;
