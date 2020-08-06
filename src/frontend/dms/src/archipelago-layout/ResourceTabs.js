13eb1daaceb5a904f9b857f5dbb9ab3b5cfeb6d8
srosset81 <srosset81@gmail.com>
2020-08-06 10:43:23 +0200

    Adapt ResourceTabs to mobiles

--- src/frontend/dms/src/archipelago-layout/ResourceTabs.js
+++ src/frontend/dms/src/archipelago-layout/ResourceTabs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { getResources } from 'react-admin';
-import { Tabs, Tab } from '@material-ui/core';
+import { Tabs, Tab, useMediaQuery } from '@material-ui/core';
 import { useHistory, useLocation } from 'react-router';
 import { shallowEqual, useSelector } from 'react-redux';
 
@@ -13,12 +13,15 @@ const ResourceTabs = () => {
 
   const resources = useSelector(getResources, shallowEqual);
 
+  const xs = useMediaQuery(theme => theme.breakpoints.down('xs'));
+
   return (
     <Tabs
       value={currentResource}
       onChange={(_, value) => history.push('/' + value)}
       indicatorColor="primary"
       textColor="primary"
+      scrollButtons="auto"
     >
       {resources
         .filter(resource => resource.hasList)
@@ -26,7 +29,7 @@ const ResourceTabs = () => {
           <Tab
             key={resource.name}
             icon={React.createElement(resource.icon)}
-            label={resource.options.label}
+            label={xs ? undefined : resource.options.label}
             value={resource.name}
           />
         ))}
