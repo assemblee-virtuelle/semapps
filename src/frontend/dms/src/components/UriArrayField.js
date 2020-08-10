0d0cea3c923bb79848a80237dfe94bdd6649e50c
srosset81 <srosset81@gmail.com>
2020-08-10 11:11:38 +0200

    Change now ENV

--- src/frontend/dms/src/components/UriArrayField.js
+++ src/frontend/dms/src/components/UriArrayField.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ReferenceArrayField } from 'react-admin';
 
-// We cannot use basePath because it is overwritten by the parent component during the cloning
 const UriArrayField = ({ record, source, ...otherProps }) => {
   if (record[source]) {
     if (Array.isArray(record[source])) {
