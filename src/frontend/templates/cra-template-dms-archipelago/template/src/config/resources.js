cd9d5f3ac27edea96be050b59c33c949424827df
simon louvet <simon.louvet.zen@gmail.com>
2020-11-10 13:04:03 +0100

    add template archipelago & updates versions template dms update person containers dms

---
+++ src/frontend/templates/cra-template-dms-archipelago/template/src/config/resources.js
@@ -0,0 +1,11 @@
+const resources = {
+
+  User: {
+    types: ['foaf:Person'],
+    containerUri: process.env.REACT_APP_MIDDLEWARE_URL + 'persons',
+    slugField: ['foaf:name', 'foaf:familyName']
+  },
+
+};
+
+export default resources;
