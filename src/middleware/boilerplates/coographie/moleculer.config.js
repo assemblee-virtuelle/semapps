46915fbd5ab6bc76daced97464642bfac0c1dd61
srosset81 <srosset81@gmail.com>
2020-11-13 11:58:12 +0100

    Set back cacher

--- src/middleware/boilerplates/coographie/moleculer.config.js
+++ src/middleware/boilerplates/coographie/moleculer.config.js
@@ -3,16 +3,16 @@ const CONFIG = require('./config');
 module.exports = {
   // You can set all ServiceBroker configurations here
   // See https://moleculer.services/docs/0.14/configuration.html
-  // cacher: {
-  //   type: 'Redis',
-  //   options: {
-  //     prefix: 'action',
-  //     ttl: 86400, // Time-to-live of one hour
-  //     redis: {
-  //       host: CONFIG.REDIS_HOST,
-  //       port: CONFIG.REDIS_PORT,
-  //       db: CONFIG.REDIS_DB
-  //     }
-  //   }
-  // }
+  cacher: {
+    type: 'Redis',
+    options: {
+      prefix: 'action',
+      ttl: 86400, // Time-to-live of one hour
+      redis: {
+        host: CONFIG.REDIS_HOST,
+        port: CONFIG.REDIS_PORT,
+        db: CONFIG.REDIS_DB
+      }
+    }
+  }
 };
