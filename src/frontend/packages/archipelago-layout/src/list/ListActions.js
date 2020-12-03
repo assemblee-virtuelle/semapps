e659391e2e12f3e5c089b4ddb7fdb957a822ed34
simon louvet <simon.louvet.zen@gmail.com>
2020-12-03 13:22:46 +0100

    display create button only if ressource have create screen

--- src/frontend/packages/archipelago-layout/src/list/ListActions.js
+++ src/frontend/packages/archipelago-layout/src/list/ListActions.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { TopToolbar, CreateButton, ExportButton, RefreshButton } from 'react-admin';
+import { TopToolbar, CreateButton, ExportButton, RefreshButton, getResources } from 'react-admin';
 import { useMediaQuery } from '@material-ui/core';
+import { shallowEqual, useSelector } from 'react-redux';
 
 // Do not show Export and Refresh buttons on mobile
 const ListActions = ({
@@ -18,6 +19,8 @@ const ListActions = ({
   total
 }) => {
   const xs = useMediaQuery(theme => theme.breakpoints.down('xs'));
+  const resources = useSelector(getResources, shallowEqual);
+  const currentResource = resources.filter(r=>r.name==resource)[0] ;
   return (
     <TopToolbar>
       {bulkActions &&
@@ -36,7 +39,9 @@ const ListActions = ({
           filterValues,
           context: 'button'
         })}
-      <CreateButton basePath={basePath} />
+      {currentResource.hasCreate &&
+        <CreateButton basePath={basePath} />
+      }
       {!xs && (
         <ExportButton
           disabled={total === 0}
