6bc5f46730f1cf1a8f5689696e48f1fd2dc4f22b
srosset81 <srosset81@gmail.com>
2020-12-10 18:44:19 +0100

    Disable auth

--- src/middleware/boilerplates/pair-ldp-server/moleculer.config.js
+++ src/middleware/boilerplates/pair-ldp-server/moleculer.config.js
@@ -3,12 +3,12 @@ const CONFIG = require('./config');
 module.exports = {
   // You can set all ServiceBroker configurations here
   // See https://moleculer.services/docs/0.14/configuration.html
-  // cacher: {
-  //   type: 'Redis',
-  //   options: {
-  //     prefix: 'action',
-  //     ttl: 2592000, // Keep in cache for one month
-  //     redis: CONFIG.REDIS_CACHE_URL
-  //   }
-  // }
+  cacher: {
+    type: 'Redis',
+    options: {
+      prefix: 'action',
+      ttl: 2592000, // Keep in cache for one month
+      redis: CONFIG.REDIS_CACHE_URL
+    }
+  }
 };
