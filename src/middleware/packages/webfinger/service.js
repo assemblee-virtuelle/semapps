124ac51564991f5618d50bfd736a8012e8d361eb
srosset81 <srosset81@gmail.com>
2021-01-28 11:29:05 +0100

    Fix WebFinger service

--- src/middleware/packages/webfinger/service.js
+++ src/middleware/packages/webfinger/service.js
@@ -1,15 +1,15 @@
-const urlJoin = require('url-join');
+const { MIME_TYPES } = require('@semapps/mime-types');
 
 const WebfingerService = {
   name: 'webfinger',
   settings: {
-    usersContainer: null,
-    domainName: null // If not set, will be extracted from usersContainer
+    baseUrl: null,
+    domainName: null // If not set, will be extracted from baseUrl
   },
-  dependencies: ['activitypub.actor'],
+  dependencies: ['triplestore', 'ldp'],
   started() {
     if (!this.settings.domainName) {
-      this.settings.domainName = new URL(this.settings.usersContainer).host;
+      this.settings.domainName = new URL(this.settings.baseUrl).host;
     }
   },
   actions: {
@@ -18,35 +18,49 @@ const WebfingerService = {
 
       const usernameMatchRegex = new RegExp(`^acct:([\\w-_.]*)@${this.settings.domainName}$`);
       const matches = resource.match(usernameMatchRegex);
+
       if (matches) {
         const userName = matches[1];
-        const userUri = urlJoin(this.settings.usersContainer, userName);
+        const result = await ctx.call('triplestore.query', {
+          query: `
+            PREFIX as: <https://www.w3.org/ns/activitystreams#>
+            SELECT ?userUri
+            WHERE {
+              ?userUri a ?type .
+              ?userUri as:preferredUsername "${userName}" .
+              FILTER( ?type IN (as:Application, as:Group, as:Organization, as:Person, as:Service) ) .
+            }
+          `,
+          accept: MIME_TYPES.JSON
+        });
 
-        let actor;
-        try {
-          actor = await ctx.call('activitypub.actor.get', { id: userUri });
-        } catch (e) {
-          actor = null;
-        }
+        if( result.length > 0 ) {
+          const userUri = result[0].userUri.value;
 
-        if (actor) {
-          return {
-            subject: resource,
-            aliases: [userUri],
-            links: [
-              {
-                rel: 'self',
-                type: 'application/activity+json',
-                href: userUri
-              }
-            ]
-          };
-        } else {
-          ctx.meta.$statusCode = 404;
+          let actor;
+          try {
+            actor = await ctx.call('ldp.resource.get', { resourceUri: userUri });
+          } catch (e) {
+            actor = null;
+          }
+
+          if (actor) {
+            return {
+              subject: resource,
+              aliases: [userUri],
+              links: [
+                {
+                  rel: 'self',
+                  type: 'application/activity+json',
+                  href: userUri
+                }
+              ]
+            };
+          }
         }
-      } else {
-        ctx.meta.$statusCode = 404;
       }
+
+      ctx.meta.$statusCode = 404;
     },
     async getRemoteUri(ctx) {
       const { account } = ctx.params;
