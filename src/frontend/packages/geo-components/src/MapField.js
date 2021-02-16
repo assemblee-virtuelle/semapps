cf87fe0a77cea4c2db7759031882ea9ce2d03523
srosset81 <srosset81@users.noreply.github.com>
2021-02-16 09:45:08 +0000

    Automatic prettier

--- src/frontend/packages/geo-components/src/MapField.js
+++ src/frontend/packages/geo-components/src/MapField.js
@@ -6,7 +6,7 @@ const MapField = ({ record, latitude, longitude, height, ...otherProps }) => {
   const position = [latitude(record), longitude(record)];
 
   // Do not display the component if it has no latitude or longitude
-  if( !position[0] || !position[1] ) return null;
+  if (!position[0] || !position[1]) return null;
 
   return (
     <MapContainer style={{ height }} center={position} {...otherProps}>
@@ -14,10 +14,10 @@ const MapField = ({ record, latitude, longitude, height, ...otherProps }) => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}/>
+      <Marker position={position} />
     </MapContainer>
   );
-}
+};
 
 MapField.defaultProps = {
   height: 400,
