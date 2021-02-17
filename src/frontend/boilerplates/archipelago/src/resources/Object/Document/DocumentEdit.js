941f4646b12929e4190cb3f87e32ca5b4ddd82d2
srosset81 <srosset81@gmail.com>
2021-02-17 19:30:51 +0100

    useLoadLinks

--- src/frontend/boilerplates/archipelago/src/resources/Object/Document/DocumentEdit.js
+++ src/frontend/boilerplates/archipelago/src/resources/Object/Document/DocumentEdit.js
@@ -1,34 +1,12 @@
 import React from 'react';
-import { SelectInput, SimpleForm, TextInput, useDataProvider } from 'react-admin';
-import { MarkdownInput } from '@semapps/markdown-components';
+import { SelectInput, SimpleForm, TextInput } from 'react-admin';
+import { MarkdownInput, useLoadLinks } from '@semapps/markdown-components';
 import { Edit, ReferenceQuickCreateInput } from '@semapps/archipelago-layout';
 import { AgentsInput } from '../../../pair';
 import DocumentTitle from './DocumentTitle';
 
 export const DocumentEdit = props => {
-  const dataProvider = useDataProvider();
-
-  const loadSuggestions = async keyword => {
-    if (keyword) {
-      const results = await dataProvider.getList('Document', {
-        pagination: {
-          page: 1,
-          perPage: 5
-        },
-        filter: { q: keyword }
-      });
-      if (results.total > 0) {
-        return results.data.map(record => ({
-          preview: record['pair:label'],
-          value: `[${record['pair:label']}](#/Document/${encodeURIComponent(record.id)}/show)`
-        }));
-      } else {
-        return [{ preview: 'Aucun résultat', value: '[' + keyword }];
-      }
-    }
-    return [{ preview: 'Type to search documents', value: '[' + keyword }];
-  };
-
+  const loadLinks = useLoadLinks('Document', 'id', 'pair:label');
   return (
     <Edit title={<DocumentTitle />} {...props}>
       <SimpleForm redirect="show">
@@ -38,7 +16,7 @@ export const DocumentEdit = props => {
           source="pair:description"
           fullWidth
           suggestionTriggerCharacters="["
-          loadSuggestions={loadSuggestions}
+          loadSuggestions={loadLinks}
         />
         <ReferenceQuickCreateInput reference="Folder" source="pair:containedIn">
           <SelectInput optionText="pair:label" />
--- src/frontend/packages/markdown-components/src/index.js
+++ src/frontend/packages/markdown-components/src/index.js
@@ -1,2 +1,3 @@
 export { default as MarkdownInput } from './MarkdownInput';
 export { default as MarkdownField } from './MarkdownField';
+export { default as useLoadLinks } from './useLoadLinks';
