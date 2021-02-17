d838fe1a064eb022b5195bcef550f2d80d6ba6e3
srosset81 <srosset81@users.noreply.github.com>
2021-02-17 08:16:09 +0000

    Automatic prettier

--- src/frontend/boilerplates/archipelago/src/resources/Agent/Activity/Event/EventEdit.js
+++ src/frontend/boilerplates/archipelago/src/resources/Agent/Activity/Event/EventEdit.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { SimpleForm, TextInput } from 'react-admin';
 import MarkdownInput from 'ra-input-markdown';
 import DateFnsUtils from '@date-io/date-fns';
-import frLocale from "date-fns/locale/fr";
+import frLocale from 'date-fns/locale/fr';
 import { Edit } from '@semapps/archipelago-layout';
 import { DateTimeInput } from '@semapps/date-components';
 import { ActorsInput, ThemesInput } from '../../../../inputs';
@@ -15,7 +15,12 @@ const EventEdit = props => (
       <TextInput source="pair:comment" fullWidth />
       <MarkdownInput multiline source="pair:description" fullWidth />
       <TextInput source="pair:aboutPage" fullWidth />
-      <DateTimeInput source="pair:startDate" options={{ format: 'DD/MM/YYYY', ampm: false }} providerOptions={{ dateAdapter: DateFnsUtils }} fullWidth />
+      <DateTimeInput
+        source="pair:startDate"
+        options={{ format: 'DD/MM/YYYY', ampm: false }}
+        providerOptions={{ dateAdapter: DateFnsUtils }}
+        fullWidth
+      />
       <DateTimeInput source="pair:endDate" options={{ format: 'DD/MM/YYYY' }} fullWidth />
       <ActorsInput source="pair:involves" />
       <ThemesInput source="pair:hasTopic" />
