882eb4e93861616f0963daeb50dab8f411fd1b9f
Niko <info@parlepeuple.fr>
2021-03-02 18:40:11 +0200

    remove # in react asmin routes + fix for login: stay at login page while going to middleware auth URL (prevents a useless reload)

--- src/frontend/boilerplates/archipelago/src/App.js
+++ src/frontend/boilerplates/archipelago/src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import { Layout, AppBar, theme } from '@semapps/archipelago-layout';
 import { authProvider, LoginPage, LogoutButton, UserMenu } from '@semapps/auth-provider';
+import { createBrowserHistory as createHistory } from 'history';
 
 import i18nProvider from './config/i18nProvider';
 import dataProvider from './config/dataProvider';
 import * as resources from './resources';
 
+const history = createHistory();
 const AppBarWithUserMenu = props => <AppBar userMenu={<UserMenu />} {...props} />;
 const LayoutWithUserMenu = props => <Layout {...props} appBar={AppBarWithUserMenu} />;
 
 const App = () => (
   <Admin
+    disableTelemetry
+    history={history}
+    title="SemApps"
     authProvider={authProvider(process.env.REACT_APP_MIDDLEWARE_URL)}
     dataProvider={dataProvider}
     i18nProvider={i18nProvider}
