935650e43d1b5d00a9c5adb4e3153e21bb46fd1f
srosset81 <srosset81@gmail.com>
2021-03-24 14:40:53 +0100

    Allow fuseki-admin.createDataset to create secure or unsecure datasets

--- src/middleware/packages/fuseki-admin/index.js
+++ src/middleware/packages/fuseki-admin/index.js
@@ -1,10 +1,7 @@
 const fetch = require('node-fetch');
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
-const templateFilePath = path.join(__dirname, 'dataset.template');
-const util = require('util');
-const readFile = util.promisify(fs.readFile);
-var format = require('string-template');
+const format = require('string-template');
 
 const delay = t => new Promise(resolve => setTimeout(resolve, t));
 
@@ -41,22 +38,33 @@ const FusekiAdminService = {
       }
     },
     async createDataset(ctx) {
-      const { dataset } = ctx.params;
+      const { dataset, secure } = ctx.params;
       const exist = await this.actions.datasetExist({ dataset });
       if (!exist) {
         console.warn(`Data ${dataset} doesn't exist. Creating it...`);
-        let template = await readFile(templateFilePath, 'utf8');
-        let assembler = format(template, { dataset: dataset });
-        const response = await fetch(this.settings.url + '$/datasets', {
-          method: 'POST',
-          headers: { ...this.headers, 'Content-Type': 'text/turtle' },
-          body: assembler
-        });
+        let response;
+
+        if( secure ) {
+          const templateFilePath = path.join(__dirname, 'templates', 'secure-dataset.ttl');
+          const template = await fsPromises.readFile(templateFilePath, 'utf8');
+          const assembler = format(template, { dataset: dataset });
+          response = await fetch(this.settings.url + '$/datasets', {
+            method: 'POST',
+            headers: { ...this.headers, 'Content-Type': 'text/turtle' },
+            body: assembler
+          });
+        } else {
+          response = await fetch(this.settings.url + '$/datasets' + '?state=active&dbType=tdb2&dbName=' + dataset, {
+            method: 'POST',
+            headers: this.headers
+          });
+        }
+
         if (response.status === 200) {
-          console.log(`Dataset ${dataset} created`);
+          console.log(`Created ${secure ? 'secure ' : ''}dataset ${dataset}`);
         } else {
           console.log(await response.text());
-          throw new Error(`Error when creating dataset ${dataset}`);
+          throw new Error(`Error when creating ${secure ? 'secure ' : ''}dataset ${dataset}`);
         }
       }
     },
