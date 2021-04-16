4584bfbaa1c97ff644d28a566cd4a9cbf1147b83
srosset81 <srosset81@users.noreply.github.com>
2021-04-16 09:54:04 +0000

    Automatic prettier

--- src/frontend/packages/auth-provider/src/components/PermissionsDialog/EditPermissionsForm.js
+++ src/frontend/packages/auth-provider/src/components/PermissionsDialog/EditPermissionsForm.js
@@ -1,22 +1,23 @@
 import React, { useMemo, useCallback } from 'react';
 import { useAuthProvider } from 'react-admin';
 import { List, makeStyles } from '@material-ui/core';
-import AgentItem from "../AgentItem";
-import { agentsDefinitions } from "../../constants";
-import usePermissionsWithRefetch from "../usePermissionsWithRefetch";
+import AgentItem from '../AgentItem';
+import { agentsDefinitions } from '../../constants';
+import usePermissionsWithRefetch from '../usePermissionsWithRefetch';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   list: {
-    width: "100%",
-    maxWidth: "100%",
+    width: '100%',
+    maxWidth: '100%',
     backgroundColor: theme.palette.background.paper
   }
 }));
 
 const defaultToArray = value => (!value ? undefined : Array.isArray(value) ? value : [value]);
-const applyToAgentClass = (p, agentClass) => Array.isArray(p['acl:agentClass']) ? p['acl:agentClass'].includes(agentClass) : p['acl:agentClass'] === agentClass;
+const applyToAgentClass = (p, agentClass) =>
+  Array.isArray(p['acl:agentClass']) ? p['acl:agentClass'].includes(agentClass) : p['acl:agentClass'] === agentClass;
 const appendPermission = (agents, agentId, agentType, p) => {
-  if( agents[agentId] ) {
+  if (agents[agentId]) {
     agents[agentId].permissions.push(p);
   } else {
     agents[agentId] = {
@@ -36,42 +37,47 @@ const EditPermissionsForm = ({ resourceId }) => {
   // Format list of authorized agents, based on the permissions returned for the resource
   const agents = useMemo(() => {
     let returnValue = {};
-    if( permissions ) {
-      for( let p of permissions ) {
-        if( applyToAgentClass(p, 'foaf:Agent') ) {
+    if (permissions) {
+      for (let p of permissions) {
+        if (applyToAgentClass(p, 'foaf:Agent')) {
           appendPermission(returnValue, 'foaf:Agent', 'anon', p['acl:mode']);
         }
-        if( applyToAgentClass(p, 'acl:AuthenticatedAgent') ) {
+        if (applyToAgentClass(p, 'acl:AuthenticatedAgent')) {
           appendPermission(returnValue, 'acl:AuthenticatedAgent', 'anyUser', p['acl:mode']);
         }
-        if( p['acl:agent'] ) {
-          defaultToArray(p['acl:agent']).forEach(userUri => appendPermission(returnValue, userUri, 'user', p['acl:mode']));
+        if (p['acl:agent']) {
+          defaultToArray(p['acl:agent']).forEach(userUri =>
+            appendPermission(returnValue, userUri, 'user', p['acl:mode'])
+          );
         }
-        if( p['acl:agentGroup'] ) {
-          defaultToArray(p['acl:agentGroup']).forEach(groupUri => appendPermission(returnValue, groupUri, 'group', p['acl:mode']));
+        if (p['acl:agentGroup']) {
+          defaultToArray(p['acl:agentGroup']).forEach(groupUri =>
+            appendPermission(returnValue, groupUri, 'group', p['acl:mode'])
+          );
         }
       }
     }
     return returnValue;
   }, [permissions]);
 
-  const addPermission = useCallback((agentId, agentType, mode) => {
-    authProvider
-      .addPermission(resourceId, agentId, agentType, mode)
-      .then(result => {
-        if( result ) {
+  const addPermission = useCallback(
+    (agentId, agentType, mode) => {
+      authProvider.addPermission(resourceId, agentId, agentType, mode).then(result => {
+        if (result) {
           refetch();
         }
       });
-  }, [resourceId, authProvider, refetch]);
+    },
+    [resourceId, authProvider, refetch]
+  );
 
-  return(
+  return (
     <List dense className={classes.list}>
       {Object.entries(agents).map(([agentId, agent]) => (
         <AgentItem key={agentId} agent={agent} addPermission={addPermission} />
       ))}
     </List>
-  )
+  );
 };
 
 export default EditPermissionsForm;
