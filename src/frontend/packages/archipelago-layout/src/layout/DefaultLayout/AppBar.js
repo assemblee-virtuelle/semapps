a0bb1d776461a61d910384c35dd941b1f304369a
VincentFarcy <v.farcy72@gmail.com>
2021-05-12 11:16:52 +0200

    [#735] add logo and title on main header

--- src/frontend/packages/archipelago-layout/src/layout/DefaultLayout/AppBar.js
+++ src/frontend/packages/archipelago-layout/src/layout/DefaultLayout/AppBar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { AppBar as RaAppBar } from 'react-admin';
-import { Hidden, makeStyles } from '@material-ui/core';
+import { Zoom, Hidden, makeStyles } from '@material-ui/core';
 import SearchForm from '../SearchForm';
-
-const useStyles = makeStyles({
+import Typography from '@material-ui/core/Typography';
+const useStyles = makeStyles((theme) => ({
   toolbar: {
     height: 56
   },
@@ -12,14 +12,52 @@ const useStyles = makeStyles({
   },
   searchForm: {
     flex: 2
-  }
-});
+  },
+  presContainer: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    minWidth: 56,
+    height: 56,
+    marginRight: 8,
+  },
+  logo: {
+    height: '100%',
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'block',
+    },
+  },
+}));
 
 const AppBar = props => {
   const classes = useStyles();
   return (
     <RaAppBar {...props} classes={{ toolbar: classes.toolbar, ...props.classes }} color="primary">
-      <span className={classes.spacer} />
+      <span className={classes.spacer} >
+        <div className={classes.presContainer}>
+            <div className={classes.logoContainer}>
+                <Zoom in={true} timeout={2000}>
+                    <img 
+                        className={classes.logo}
+                        src={process.env.PUBLIC_URL + '/logo192.png'}
+                        alt="logo"
+                    />
+                </Zoom>
+            </div>
+            <Typography
+                className={classes.title}
+                variant="h6"
+                noWrap
+            >
+                { props.title }
+            </Typography>
+        </div> 
+      </span>
       <Hidden only="xs">
         <span className={classes.searchForm}>
           <SearchForm />
