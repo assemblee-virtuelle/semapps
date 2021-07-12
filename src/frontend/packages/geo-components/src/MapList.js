d1e7c139106b06c35ea2b853ce617d8ab867180b
srosset81 <srosset81@gmail.com>
2021-07-12 19:52:43 +0200

    Improve MapList component

--- src/frontend/packages/geo-components/src/MapList.js
+++ src/frontend/packages/geo-components/src/MapList.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useListContext } from 'react-admin';
 import { useLocation } from 'react-router';
+import { useMediaQuery, Drawer, Box, IconButton, makeStyles } from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 import DefaultPopupContent from './DefaultPopupContent';
 import QueryStringUpdater from './QueryStringUpdater';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles(() => ({
+  loading: {
+    zIndex: 1000,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  closeButton: {
+    position: 'absolute',
+    zIndex: 1400,
+    top: 0,
+    right: 0
+  }
+}));
 
 const MapList = ({
   latitude,
@@ -19,7 +42,11 @@ const MapList = ({
   connectMarkers,
   ...otherProps
 }) => {
-  const { ids, data, basePath } = useListContext();
+  const { ids, data, basePath, loading } = useListContext();
+  const xs = useMediaQuery(theme => theme.breakpoints.down('xs'), { noSsr: true });
+  const [drawerRecord, setDrawerRecord] = useState(null);
+  const [map, setMap] = useState(null);
+  const classes = useStyles();
 
   // Get the zoom and center from query string, if available
   const query = new URLSearchParams(useLocation().search);
@@ -50,18 +77,36 @@ const MapList = ({
       center={!boundToMarkers ? center : undefined}
       zoom={!boundToMarkers ? zoom : undefined}
       bounds={bounds}
+      whenCreated={setMap}
       {...otherProps}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {loading && (
+        <Box alignItems="center" className={classes.loading}>
+          <CircularProgress size={60} thickness={6} />
+        </Box>
+      )}
       <MarkerClusterGroup showCoverageOnHover={false}>
         {records.map((record, i) => {
           const marker = (
             <React.Fragment key={i}>
-              <Marker position={[record.latitude, record.longitude]}>
-                <Popup>{React.createElement(popupContent, { record, basePath })}</Popup>
+              <Marker
+                position={[record.latitude, record.longitude]}
+                eventHandlers={
+                  xs
+                    ? {
+                        click: () => {
+                          map.setView([record.latitude, record.longitude]);
+                          setDrawerRecord(record);
+                        }
+                      }
+                    : undefined
+                }
+              >
+                {!xs && <Popup>{React.createElement(popupContent, { record, basePath })}</Popup>}
               </Marker>
               {connectMarkers && previousRecord && (
                 <Polyline
@@ -81,6 +126,14 @@ const MapList = ({
         })}
       </MarkerClusterGroup>
       <QueryStringUpdater />
+      <Drawer anchor="bottom" open={!!drawerRecord} onClose={() => setDrawerRecord(null)}>
+        <Box p={1} position="relative">
+          <IconButton onClick={() => setDrawerRecord(null)} className={classes.closeButton}>
+            <CloseIcon />
+          </IconButton>
+          {drawerRecord && React.createElement(popupContent, { record: drawerRecord, basePath })}
+        </Box>
+      </Drawer>
     </MapContainer>
   );
 };
