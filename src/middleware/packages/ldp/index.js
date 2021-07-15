bbca67f28e9246d1ee9612aa7fc3bf2af377324f
srosset81 <srosset81@gmail.com>
2021-07-15 12:28:35 +0200

    Missing import

--- src/middleware/packages/ldp/index.js
+++ src/middleware/packages/ldp/index.js
@@ -4,6 +4,7 @@ module.exports = {
   LdpService: require('./service'),
   LdpContainerService: require('./services/container'),
   LdpResourceService: require('./services/resource'),
+  ResourcesWatcherBot: require('./bots/resources-watcher'),
   TripleStoreAdapter: require('./adapter'),
   getContainerRoutes: require('./routes/getContainerRoutes'),
   ...utils
