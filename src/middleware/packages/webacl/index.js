f3107559734eeceae9d1fbf5b1ea897ff7a5d825
srosset81 <srosset81@gmail.com>
2021-07-31 17:11:04 +0200

    AuthorizerBot

--- src/middleware/packages/webacl/index.js
+++ src/middleware/packages/webacl/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   GroupsManagerBot: require('./bots/groups-manager'),
+  AuthorizerBot: require('./bots/authorizer'),
   WebAclService: require('./service'),
   WebAclMiddleware: require('./middlewares/webacl'),
   CacherMiddleware: require('./middlewares/cacher')
