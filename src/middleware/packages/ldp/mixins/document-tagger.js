8a8738e52671d091b178814f399ee50921720de9
srosset81 <srosset81@gmail.com>
2021-08-02 11:10:56 +0200

    Simplify usage to avoid requiring DC ontology and to avoid double call to ldp.resource.patch

--- src/middleware/packages/ldp/mixins/document-tagger.js
+++ src/middleware/packages/ldp/mixins/document-tagger.js
@@ -1,65 +1,53 @@
-const { MIME_TYPES } = require('@semapps/mime-types');
-
 module.exports = {
   settings: {
     documentPredicates: {
-      created: 'dc:created',
-      updated: 'dc:modified',
-      creator: 'dc:creator'
+      created: 'http://purl.org/dc/terms/created',
+      updated: 'http://purl.org/dc/terms/modified',
+      creator: 'http://purl.org/dc/terms/creator'
     }
   },
   actions: {
-    tagCreatedResource(ctx) {
+    async tagCreatedResource(ctx) {
       const { resourceUri, webId } = ctx.params;
       const now = new Date();
-      ctx.call('ldp.resource.patch', {
-        resource: {
-          '@id': resourceUri,
-          [this.settings.documentPredicates.created]: now.toISOString(),
-          [this.settings.documentPredicates.updated]: now.toISOString(),
-          [this.settings.documentPredicates.creator]: webId
-        },
-        contentType: MIME_TYPES.JSON,
-        webId
+
+      let triples = [];
+      triples.push(`<${resourceUri}> <${this.settings.documentPredicates.created}> "${now.toISOString()}"^^xsd:dateTime .`);
+      triples.push(`<${resourceUri}> <${this.settings.documentPredicates.updated}> "${now.toISOString()}"^^xsd:dateTime .`);
+      if( webId && webId.startsWith('http') ) triples.push(`<${resourceUri}> <${this.settings.documentPredicates.creator}> <${webId}> .`);
+
+      await ctx.call('triplestore.insert', {
+        resource: triples.join('\n'),
+        webId: 'system'
       });
     },
-    tagUpdatedResource(ctx) {
-      const { resourceUri, webId } = ctx.params;
+    async tagUpdatedResource(ctx) {
+      const { resourceUri } = ctx.params;
       const now = new Date();
-      ctx.call('ldp.resource.patch', {
-        resource: {
-          '@id': resourceUri,
-          [this.settings.documentPredicates.updated]: now.toISOString()
-        },
-        contentType: MIME_TYPES.JSON,
-        webId
-      });
+      await ctx.call('triplestore.update', {
+        query: `
+          DELETE { <${resourceUri}> <${this.settings.documentPredicates.updated}> ?updated }
+          INSERT { <${resourceUri}> <${this.settings.documentPredicates.updated}> "${now.toISOString()}"^^xsd:dateTime }
+          WHERE { <${resourceUri}> <${this.settings.documentPredicates.updated}> ?updated }
+        `,
+        webId: 'system'
+      })
     }
   },
   events: {
     async 'ldp.resource.created'(ctx) {
       const { resourceUri, webId } = ctx.params;
       this.actions.tagCreatedResource(
-        {
-          resourceUri,
-          webId
-        },
+        { resourceUri, webId },
         { parentCtx: ctx }
       );
     },
     async 'ldp.resource.updated'(ctx) {
-      const { resourceUri, newData, oldData, webId } = ctx.params;
-
-      // Do not update modified date if it is being changed here (prevent infinite loop)
-      if (newData[this.settings.documentPredicates.updated] === oldData[this.settings.documentPredicates.updated]) {
-        this.actions.tagUpdatedResource(
-          {
-            resourceUri,
-            webId
-          },
-          { parentCtx: ctx }
-        );
-      }
+      const { resourceUri } = ctx.params;
+      this.actions.tagUpdatedResource(
+        { resourceUri },
+        { parentCtx: ctx }
+      );
     }
   }
 };
