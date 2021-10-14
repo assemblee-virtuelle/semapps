38b2b9925824eb97c6eda05b957995b4f9c4817f
srosset81 <srosset81@users.noreply.github.com>
2021-10-14 14:22:44 +0000

    Automatic prettier

--- src/middleware/packages/auth/services/jwt.js
+++ src/middleware/packages/auth/services/jwt.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 
