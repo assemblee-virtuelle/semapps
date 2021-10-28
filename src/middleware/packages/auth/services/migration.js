572e1c1d2bc8ed9d4de7c551c0a202b0fb50a052
srosset81 <srosset81@users.noreply.github.com>
2021-10-28 09:01:11 +0000

    Automatic prettier

--- src/middleware/packages/auth/services/migration.js
+++ src/middleware/packages/auth/services/migration.js
@@ -1,4 +1,4 @@
-const { MIME_TYPES } = require("@semapps/mime-types");
+const { MIME_TYPES } = require('@semapps/mime-types');
 
 module.exports = {
   name: 'auth.migration',
@@ -9,8 +9,12 @@ module.exports = {
       const results = await ctx.call('ldp.container.get', { containerUri: usersContainer, accept: MIME_TYPES.JSON });
 
       for (let user of results['ldp:contains']) {
-        if( user[emailPredicate] ) {
-          await ctx.call('auth.account.create', { email: user[emailPredicate], username: user[usernamePredicate], webId: user.id })
+        if (user[emailPredicate]) {
+          await ctx.call('auth.account.create', {
+            email: user[emailPredicate],
+            username: user[usernamePredicate],
+            webId: user.id
+          });
         } else {
           console.log('No email found for user ' + user.id);
         }
