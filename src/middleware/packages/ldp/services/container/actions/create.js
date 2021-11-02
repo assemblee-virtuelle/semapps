f2e06b4d73bbe5af0c7e51645bec5493bb0740a3
srosset81 <srosset81@gmail.com>
2021-11-02 09:59:51 +0100

    Automatically attach container to parent container

--- src/middleware/packages/ldp/services/container/actions/create.js
+++ src/middleware/packages/ldp/services/container/actions/create.js
@@ -1,4 +1,5 @@
 const { MIME_TYPES } = require('@semapps/mime-types');
+const { getContainerFromUri } = require("../../../utils");
 
 module.exports = {
   visibility: 'public',
@@ -7,15 +8,27 @@ module.exports = {
     webId: { type: 'string', optional: true }
   },
   async handler(ctx) {
+    const { containerUri } = ctx.params;
     const webId = ctx.params.webId || ctx.meta.webId || 'anon';
+
     await ctx.call('triplestore.insert', {
       resource: {
         '@context': 'http://www.w3.org/ns/ldp',
-        '@id': ctx.params.containerUri,
+        '@id': containerUri,
         '@type': ['Container', 'BasicContainer']
       },
       contentType: MIME_TYPES.JSON,
       webId
     });
+
+    // Attach to parent container, if it exists
+    const parentContainerUri = getContainerFromUri(containerUri);
+    const parentContainerExist = await ctx.call('ldp.container.exist', { containerUri: parentContainerUri });
+    if( parentContainerExist ) {
+      await ctx.call('ldp.container.attach', {
+        containerUri: parentContainerUri,
+        resourceUri: containerUri
+      })
+    }
   }
 };
