74d522b831806fa80c68feb85ecec295e8b58393
srosset81 <srosset81@gmail.com>
2021-11-15 09:42:04 +0100

    Finish registry service and clean up actor service

--- src/middleware/packages/activitypub/services/registry.js
+++ src/middleware/packages/activitypub/services/registry.js
@@ -1,7 +1,8 @@
 const urlJoin = require("url-join");
 const { MIME_TYPES } = require('@semapps/mime-types');
 const { getCollectionRoute } = require("../routes/getCollectionRoute");
-const {defaultToArray} = require("../utils");
+const { defaultToArray } = require("../utils");
+const {getContainerFromUri} = require("@semapps/ldp");
 
 const CollectionRegistryService = {
   name: 'activitypub.registry',
@@ -13,7 +14,7 @@ const CollectionRegistryService = {
   dependencies: ['triplestore', 'ldp.resource'],
   async started() {
     this.registeredContainers = {};
-    this.registeredCollections = {};
+    this.registeredCollections = [];
   },
   actions: {
     async register(ctx) {
@@ -23,21 +24,27 @@ const CollectionRegistryService = {
       // Ignore undefined options
       Object.keys(options).forEach(key => (options[key] === undefined || options[key] === null) && delete options[key]);
 
+      // Save the collection locally
+      this.registeredCollections.push({ path, name, attachToTypes, ...options });
+
       // Find all containers where we want to attach this collection
-      const containers = await ctx.call('ldp.registry.getByType', { type: attachToTypes });
+      const containers = this.getContainersByType(attachToTypes);
 
       // Go through each container and add a corresponding API route
       for( let container of containers ) {
-        const collectionUri = this.settings.podProvider
-          ? urlJoin(this.settings.baseUrl, ':username', container.path, ':objectId', path)
-          : urlJoin(this.settings.baseUrl, container.path, ':objectId', path);
-
-        await this.broker.call('api.addRoute', { route: getCollectionRoute(collectionUri, options.controlledActions) });
+        await this.actions.addApiRoute({ collection: ctx.params, container });
       }
-
-      // Save the options
-      this.registeredCollections[name] = { path, name, attachToTypes, ...options };
     },
+    async addApiRoute(ctx) {
+      const { container, collection } = ctx.params;
+
+      const collectionUri = this.settings.podProvider
+        ? urlJoin(this.settings.baseUrl, ':username', container.path, ':objectId', collection.path)
+        : urlJoin(this.settings.baseUrl, container.path, ':objectId', collection.path);
+
+      // TODO ensure it's not a problem if the same route is added twice
+      await this.broker.call('api.addRoute', { route: getCollectionRoute(collectionUri, collection.controlledActions) });
+    }
     // async getUri(ctx) {
     //   const { path, webId } = ctx.params;
     //
@@ -68,43 +75,65 @@ const CollectionRegistryService = {
         });
       }
     },
+    async deleteCollection(ctx, objectUri, collection) {
+      const collectionUri = urlJoin(objectUri, collection.path);
+      const exists = await ctx.call('activitypub.collection.exist', { collectionUri, webId: 'system' });
+      if (exists) {
+        // Delete the collection
+        await ctx.call('activitypub.collection.remove', { collectionUri, webId: 'system' });
+      }
+    },
     // Get the collections attached to the given type
-    getByType(types) {
+    getCollectionsByType(types) {
       return this.registeredCollections.filter(collection => {
         defaultToArray(types).some(type =>
           Array.isArray(collection.attachToTypes)
-            ? container.attachToTypes.includes(type)
-            : container.attachToTypes === type
+            ? collection.attachToTypes.includes(type)
+            : collection.attachToTypes === type
         )
       })
+    },
+    // Get the containers with resources of the given type
+    // Same action as ldp.registry.getByType, but search through locally registered containers to avoid race conditions
+    getContainersByType(types) {
+      return Object.values(this.registeredContainers).find(container =>
+        defaultToArray(types).some(type =>
+          Array.isArray(container.acceptedTypes)
+            ? container.acceptedTypes.includes(type)
+            : container.acceptedTypes === type
+        )
+      );
     }
   },
   events: {
     async 'ldp.resource.created'(ctx) {
       const { newData } = ctx.params;
-      const collections = this.getByType(newData.type || newData['@types']);
+      const collections = this.getCollectionsByType(newData.type || newData['@types']);
       for( let collection of collections ) {
         await this.createAndAttachCollection(ctx, newData.id || newData['@id'], collection);
+      }
+    },
+    async 'ldp.resource.deleted'(ctx) {
+      const { oldData } = ctx.params;
+      const collections = this.getCollectionsByType(oldData.type || oldData['@types']);
+      for( let collection of collections ) {
+        await this.deleteCollection(ctx, newData.id || newData['@id'], collection);
       }
     },
     async 'ldp.registry.registered'(ctx) {
       const { container } = ctx.params;
 
-      // TODO: if not all collection have been registered, this list won't be complete
-      const collections = this.getByType(container.acceptedTypes);
-
-      for( let collection of collections ) {
-        const collectionUri = this.settings.podProvider
-          ? urlJoin(this.settings.baseUrl, ':username', container.path, ':objectId', collection.path)
-          : urlJoin(this.settings.baseUrl, container.path, ':objectId', collection.path);
-
-        // TODO ensure it's not a problem if the same route is added twice
-        await this.broker.call('api.addRoute', { route: getCollectionRoute(collectionUri, collection.controlledActions) });
-      }
-
       // Register the container locally
-      // TODO see if we need this
+      // Avoid race conditions, if this event is called while the register action is still running
       this.registeredContainers[container.name] = container;
+
+      // Find the collections that must be attached to the container's resources
+      const collections = this.getCollectionsByType(container.acceptedTypes);
+
+      // Go through each collection and add a corresponding API route
+      for( let collection of collections ) {
+        await this.actions.addApiRoute({ collection, container });
+      }
     }
   }
 };
