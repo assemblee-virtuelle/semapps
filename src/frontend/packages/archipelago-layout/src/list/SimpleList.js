319245c002cc23327c74c95722553d555e83947b
srosset81 <srosset81@users.noreply.github.com>
2021-12-09 10:31:27 +0000

    Automatic prettier

--- src/frontend/packages/archipelago-layout/src/list/SimpleList.js
+++ src/frontend/packages/archipelago-layout/src/list/SimpleList.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { SimpleList as RaSimpleList } from 'react-admin';
 
 const SimpleList = props => (
-  <RaSimpleList {...props} rowStyle={() => ({
-    padding: 15,
-    paddingBottom: 15,
-    paddingTop: 15,
-    marginBottom: 10,
-    borderStyle: 'solid',
-    borderColor: '#e0e0e0',
-    borderWidth: 1
-  })} />
+  <RaSimpleList
+    {...props}
+    rowStyle={() => ({
+      padding: 15,
+      paddingBottom: 15,
+      paddingTop: 15,
+      marginBottom: 10,
+      borderStyle: 'solid',
+      borderColor: '#e0e0e0',
+      borderWidth: 1
+    })}
+  />
 );
 
 export default SimpleList;
