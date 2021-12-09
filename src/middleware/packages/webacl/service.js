551acb1fec788434f025a6ce98d00c12c630912e
srosset81 <srosset81@gmail.com>
2021-12-09 14:44:07 +0100

    Fix WebACL routes to avoid path collision

--- src/middleware/packages/webacl/service.js
+++ src/middleware/packages/webacl/service.js
@@ -1,7 +1,7 @@
 const WebAclResourceService = require('./services/resource');
 const WebAclGroupService = require('./services/group');
 const WebAclCacheService = require('./services/cache');
-const { parseHeader, negotiateContentType, negotiateAccept } = require('@semapps/middlewares');
+const getRoutes = require('./routes/getRoutes');
 
 module.exports = {
   name: 'webacl',
@@ -38,67 +38,8 @@ module.exports = {
     }
   },
   async started() {
-    const routes = await this.actions.getApiRoutes();
-    for (let route of routes) {
+    for (let route of getRoutes()) {
       await this.broker.call('api.addRoute', { route });
-    }
-  },
-  actions: {
-    async getApiRoutes(ctx) {
-      const middlewares = [parseHeader, negotiateContentType, negotiateAccept];
-
-      return [
-        {
-          authorization: false,
-          authentication: true,
-          bodyParsers: {
-            json: false,
-            urlencoded: false,
-            text: {
-              type: ['text/turtle', 'application/ld+json']
-            }
-          },
-          onBeforeCall(ctx, route, req) {
-            ctx.meta.body = req.body;
-          },
-          aliases: {
-            'PATCH /_acl/:slugParts*': [parseHeader, 'webacl.resource.api_addRights'],
-            'PUT /_acl/:slugParts*': [parseHeader, 'webacl.resource.api_setRights']
-          },
-          onError(req, res, err) {
-            let { type, code, message, data, name } = err;
-            res.writeHead(Number(code) || 500, data && data.status ? data.status : 'Server error', {
-              'Content-Type': 'application/json'
-            });
-            res.end(JSON.stringify({ type, code, message, data, name }));
-          }
-        },
-        {
-          authorization: false,
-          authentication: true,
-          aliases: {
-            'GET /_acl/:slugParts*': [...middlewares, 'webacl.resource.api_getRights'],
-            'GET /_rights/:slugParts*': [...middlewares, 'webacl.resource.api_hasRights'],
-            'POST /_rights/:slugParts*': [...middlewares, 'webacl.resource.api_hasRights'],
-            'PATCH /_groups/:id': ['webacl.group.api_addMember'],
-            'POST /_groups': ['webacl.group.api_create'],
-            'GET /_groups/:id': ['webacl.group.api_getMembers'],
-            'GET /_groups': ['webacl.group.api_getGroups'],
-            'DELETE /_groups/:id': ['webacl.group.api_delete'],
-            'POST /_groups/:id': ['webacl.group.api_removeMember']
-          },
-          bodyParsers: {
-            json: true
-          },
-          onError(req, res, err) {
-            let { type, code, message, data, name } = err;
-            res.writeHead(Number(code) || 500, data && data.status ? data.status : 'Server error', {
-              'Content-Type': 'application/json'
-            });
-            res.end(JSON.stringify({ type, code, message, data, name }));
-          }
-        }
-      ];
     }
   }
 };
