199cbc0a93a59778ec4031cf52fcdbf6bb380394
Vincentfarcy <v.farcy72@gmail.com>
2021-12-27 12:09:32 +0100

    fix getManyReference

--- src/frontend/packages/semantic-data-provider/src/dataProvider/methods/getManyReference.js
+++ src/frontend/packages/semantic-data-provider/src/dataProvider/methods/getManyReference.js
@@ -3,7 +3,7 @@ import getList from './getList';
 const getManyReferenceMethod = config => async (resourceId, params) => {
   params.filter = { ...params.filter, [params.target]: params.id };
   delete params.target;
-  return await getList(config)(params);
+  return await getList(config)(resourceId, params);
 };
 
 export default getManyReferenceMethod;
