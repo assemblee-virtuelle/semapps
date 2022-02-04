3c5207bac1d8faa190a067b30f6f8e7e67bc5c2e
srosset81 <srosset81@gmail.com>
2022-02-04 15:53:19 +0100

    Fix #950 and improve dataModel.list.containers handling

---
+++ src/frontend/packages/semantic-data-provider/src/dataProvider/utils/findContainersWithPath.js
@@ -0,0 +1,18 @@
+import urlJoin from 'url-join';
+
+const findContainersWithPaths = (paths, dataServers) => {
+  let containers = {};
+  Object.keys(paths).forEach(serverKey => {
+    if( dataServers[serverKey] ) {
+      containers[serverKey] = [];
+      paths[serverKey].forEach(path => {
+        containers[serverKey].push(urlJoin(dataServers[serverKey].baseUrl, path))
+      });
+    } else {
+      throw new Error('No server found with key ' + serverKey);
+    }
+  });
+  return containers;
+};
+
+export default findContainersWithPaths;
