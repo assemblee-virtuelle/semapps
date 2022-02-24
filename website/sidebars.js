fbf735c9b70d7eedb6015f9328bd0890e5a17ff5
Yaninck DUTHE <yannickduthe@hotmail.com>
2022-02-24 18:39:33 +0100

    Change left menus for guides/middleware/frontend

--- website/sidebars.js
+++ website/sidebars.js
@@ -6,49 +6,47 @@
  */
 
 module.exports = {
-  docs: {
-    Guides: ['guides/ldp-server','guides/dms','guides/activitypub'],
-    'Middleware packages': [
-      'middleware/activitypub',
-      'middleware/auth',
-      'middleware/backup',
-      'middleware/fuseki-admin',
-      'middleware/importer',
-      'middleware/inference',
-      {
-        type: 'category',
-        label: 'LDP',
-        items: [
-          'middleware/ldp/index',
-          'middleware/ldp/resource',
-          'middleware/ldp/container',
-          'middleware/ldp/document-tagger',
-        ],
-      },
-      'middleware/signature',
-      'middleware/triplestore',
-      {
-        type: 'category',
-        label: 'WebACL',
-        items: [
-          'middleware/webacl/index',
-          'middleware/webacl/resource',
-          'middleware/webacl/group',
-          'middleware/webacl/authorizer',
-          'middleware/webacl/groups-manager',
-        ],
-      },
-      'middleware/webfinger',
-      'middleware/webhooks',
-      'middleware/webid'
-    ],
-    'Frontend packages': [
-      'frontend/auth-provider',
-      'frontend/date-components',
-      'frontend/geo-components',
-      'frontend/markdown-components'
-    ]
-  },
+  guides: ['guides/ldp-server','guides/dms','guides/activitypub'],
+  middleware: [
+    'middleware/activitypub',
+    'middleware/auth',
+    'middleware/backup',
+    'middleware/fuseki-admin',
+    'middleware/importer',
+    'middleware/inference',
+    {
+      type: 'category',
+      label: 'LDP',
+      items: [
+        'middleware/ldp/index',
+        'middleware/ldp/resource',
+        'middleware/ldp/container',
+        'middleware/ldp/document-tagger',
+      ],
+    },
+    'middleware/signature',
+    'middleware/triplestore',
+    {
+      type: 'category',
+      label: 'WebACL',
+      items: [
+        'middleware/webacl/index',
+        'middleware/webacl/resource',
+        'middleware/webacl/group',
+        'middleware/webacl/authorizer',
+        'middleware/webacl/groups-manager',
+      ],
+    },
+    'middleware/webfinger',
+    'middleware/webhooks',
+    'middleware/webid'
+  ],
+  Frontend: [
+    'frontend/auth-provider',
+    'frontend/date-components',
+    'frontend/geo-components',
+    'frontend/markdown-components'
+  ],
   contribute: {
     'SemApps core': ['contribute/code','contribute/coding-conventions'],
     Documentation: ['contribute/documentation','contribute/style-guide']
