cd64b7700783b8cec92b0c42f2cc45e2c4b6ee16
srosset81 <srosset81@users.noreply.github.com>
2022-02-24 17:55:35 +0000

    Automatic prettier

--- src/frontend/packages/field-components/src/QuickAppendReferenceArrayField/ResultsList.js
+++ src/frontend/packages/field-components/src/QuickAppendReferenceArrayField/ResultsList.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import {
   getResources,
   useDataProvider,
@@ -6,10 +6,21 @@ import {
   linkToRecord,
   useGetResourceLabel,
   useTranslate
-} from "react-admin";
-import { shallowEqual, useSelector } from "react-redux";
+} from 'react-admin';
+import { shallowEqual, useSelector } from 'react-redux';
 import debounce from 'lodash.debounce';
-import { Box, IconButton, List, ListItem, ListItemAvatar, ListItemText, ListItemSecondaryAction, Avatar, makeStyles, CircularProgress } from '@material-ui/core';
+import {
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  ListItemSecondaryAction,
+  Avatar,
+  makeStyles,
+  CircularProgress
+} from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import ErrorIcon from '@material-ui/icons/Error';
@@ -17,8 +28,8 @@ import { useDataServers, useDataModel } from '@semapps/semantic-data-provider';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: "100%",
-    maxWidth: "100%",
+    width: '100%',
+    maxWidth: '100%',
     backgroundColor: theme.palette.background.paper,
     paddingTop: 0,
     paddingBottom: 0
@@ -27,8 +38,8 @@ const useStyles = makeStyles(theme => ({
     width: '30%'
   },
   secondaryText: {
-    fontStyle: "italic",
-    color: "grey"
+    fontStyle: 'italic',
+    color: 'grey'
   }
 }));
 
@@ -54,28 +65,36 @@ const ResultsList = ({ keyword, source, reference, appendLink, switchToCreate })
   const getResourceLabel = useGetResourceLabel();
   const dataModel = useDataModel(reference);
 
-  if( dataModel && Object.keys(dataModel).length > 0 && !dataModel?.fieldsMapping?.title ) {
+  if (dataModel && Object.keys(dataModel).length > 0 && !dataModel?.fieldsMapping?.title) {
     throw new Error(`No fieldsMapping.title config found for ${reference} dataModel`);
   }
 
   const search = useMemo(
     () =>
-      debounce((keyword) => {
-        dataProvider.getList(reference, {
-          pagination: { page: 1, perPage: 100 },
-          sort: { field: dataModel.fieldsMapping.title, order: 'ASC' },
-          filter: { q: keyword }
-        }).then(( { data }) => {
-          const existingLinks = record[source] ? Array.isArray(record[source]) ? record[source] : [record[source]] : [];
-          const newLinks = data.filter(record => !existingLinks.includes(record.id));
-          setResults(newLinks);
-          setLoaded(true);
-          setLoading(false);
-        }).catch(e => {
-          setLoading(false);
-        })
-      }, 700)
-    , [dataProvider, dataModel, record, source, reference, setResults, setLoading, setLoaded])
+      debounce(keyword => {
+        dataProvider
+          .getList(reference, {
+            pagination: { page: 1, perPage: 100 },
+            sort: { field: dataModel.fieldsMapping.title, order: 'ASC' },
+            filter: { q: keyword }
+          })
+          .then(({ data }) => {
+            const existingLinks = record[source]
+              ? Array.isArray(record[source])
+                ? record[source]
+                : [record[source]]
+              : [];
+            const newLinks = data.filter(record => !existingLinks.includes(record.id));
+            setResults(newLinks);
+            setLoaded(true);
+            setLoading(false);
+          })
+          .catch(e => {
+            setLoading(false);
+          });
+      }, 700),
+    [dataProvider, dataModel, record, source, reference, setResults, setLoading, setLoaded]
+  );
 
   useEffect(() => {
     if (!keyword) {
@@ -85,53 +104,58 @@ const ResultsList = ({ keyword, source, reference, appendLink, switchToCreate })
       setLoaded(false);
       search(keyword);
     }
-    return(() => search.cancel());
+    return () => search.cancel();
   }, [keyword, search, setLoading]);
 
-  if( !keyword ) return null;
+  if (!keyword) return null;
 
   return (
     <List dense className={classes.root}>
-      {loaded && results.map((resource) => (
-        <ListItem key={resource.id} button onClick={() => appendLink(resource.id)}>
-          <ListItemAvatar>
-            <Avatar>{React.createElement(referenceDefinition.icon)}</Avatar>
-          </ListItemAvatar>
-          <ListItemText className={classes.primaryText} primary={resource[dataModel.fieldsMapping.title]} />
-          <ListItemText
-            className={classes.secondaryText}
-            primary={getServerName(resource.id, dataServers)}
-          />
-          <ListItemSecondaryAction>
-            <a href={linkToRecord('/' + reference, resource.id, 'show')} target="_blank" rel="noopener noreferrer">
-              <IconButton edge="end">
-                <VisibilityIcon />
-              </IconButton>
-            </a>
-          </ListItemSecondaryAction>
-        </ListItem>
-      ))}
-      {loaded && results.length === 0 &&
+      {loaded &&
+        results.map(resource => (
+          <ListItem key={resource.id} button onClick={() => appendLink(resource.id)}>
+            <ListItemAvatar>
+              <Avatar>{React.createElement(referenceDefinition.icon)}</Avatar>
+            </ListItemAvatar>
+            <ListItemText className={classes.primaryText} primary={resource[dataModel.fieldsMapping.title]} />
+            <ListItemText className={classes.secondaryText} primary={getServerName(resource.id, dataServers)} />
+            <ListItemSecondaryAction>
+              <a href={linkToRecord('/' + reference, resource.id, 'show')} target="_blank" rel="noopener noreferrer">
+                <IconButton edge="end">
+                  <VisibilityIcon />
+                </IconButton>
+              </a>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      {loaded && results.length === 0 && (
         <ListItem>
           <ListItemAvatar>
-            <Avatar><ErrorIcon /></Avatar>
+            <Avatar>
+              <ErrorIcon />
+            </Avatar>
           </ListItemAvatar>
           <ListItemText className={classes.primaryText} primary={translate('ra.navigation.no_results')} />
         </ListItem>
-      }
-      {loaded && referenceDefinition.hasCreate &&
+      )}
+      {loaded && referenceDefinition.hasCreate && (
         <ListItem button onClick={switchToCreate}>
           <ListItemAvatar>
-            <Avatar><AddIcon /></Avatar>
+            <Avatar>
+              <AddIcon />
+            </Avatar>
           </ListItemAvatar>
-          <ListItemText className={classes.primaryText} primary={translate('ra.page.create', { name: getResourceLabel(reference, 1) })} />
+          <ListItemText
+            className={classes.primaryText}
+            primary={translate('ra.page.create', { name: getResourceLabel(reference, 1) })}
+          />
         </ListItem>
-      }
-      {loading &&
+      )}
+      {loading && (
         <Box display="flex" alignItems="center" justifyContent="center" height={150}>
           <CircularProgress size={60} thickness={6} />
         </Box>
-      }
+      )}
     </List>
   );
 };
--- src/frontend/packages/input-components/src/MultiServerAutocompleteArrayInput.js
+++ src/frontend/packages/input-components/src/MultiServerAutocompleteArrayInput.js
@@ -1,26 +1,32 @@
-import React, { useCallback } from "react";
-import { AutocompleteArrayInput } from "react-admin";
+import React, { useCallback } from 'react';
+import { AutocompleteArrayInput } from 'react-admin';
 import { useDataServers } from '@semapps/semantic-data-provider';
 
 const MultiServerAutocompleteArrayInput = ({ optionText, ...rest }) => {
   const dataServers = useDataServers();
-  const optionTextWithServerName = useCallback(record => {
-    if( record ) {
-      const server = Object.values(dataServers).find(server => record.id.startsWith(server.baseUrl));
-      return <span>{record[optionText]}{server && <em className="serverName" style={{ color: 'grey' }}>&nbsp;({server.name})</em>}</span>;
-    }
-  }, [optionText, dataServers]);
+  const optionTextWithServerName = useCallback(
+    record => {
+      if (record) {
+        const server = Object.values(dataServers).find(server => record.id.startsWith(server.baseUrl));
+        return (
+          <span>
+            {record[optionText]}
+            {server && (
+              <em className="serverName" style={{ color: 'grey' }}>
+                &nbsp;({server.name})
+              </em>
+            )}
+          </span>
+        );
+      }
+    },
+    [optionText, dataServers]
+  );
   const matchSuggestion = useCallback(
     (filterValue, choice) => choice[optionText].toLowerCase().match(filterValue.toLocaleString()),
     [optionText]
   );
-  return(
-    <AutocompleteArrayInput
-      matchSuggestion={matchSuggestion}
-      optionText={optionTextWithServerName}
-      {...rest}
-    />
-  )
+  return <AutocompleteArrayInput matchSuggestion={matchSuggestion} optionText={optionTextWithServerName} {...rest} />;
 };
 
 export default MultiServerAutocompleteArrayInput;
