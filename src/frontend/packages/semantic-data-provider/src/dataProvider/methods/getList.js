4efb0ad7c2e7012679c352b64357b3a06dfc5d4c
srosset81 <srosset81@gmail.com>
2022-03-17 12:01:19 +0100

    Improve error detection for list.containers property of dataModel

--- src/frontend/packages/semantic-data-provider/src/dataProvider/methods/getList.js
+++ src/frontend/packages/semantic-data-provider/src/dataProvider/methods/getList.js
@@ -10,7 +10,8 @@ const getListMethod = config => async (resourceId, params = {}) => {
   if (!dataModel) throw new Error(`Resource ${resourceId} is not mapped in resources file`);
 
   let containers;
-  if (!params.filter?._servers && dataModel.list?.containers && Object.keys(dataModel.list.containers).length > 0) {
+  if (!params.filter?._servers && dataModel.list?.containers) {
+    if( Array.isArray(dataModel.list?.containers) ) throw new Error(`The list.containers property of ${resourceId} dataModel must be of type object ({ serverKey: [containerUri] })`)
     // If containers are set explicitly, use them
     containers = findContainersWithPaths(dataModel.list.containers, dataServers);
   } else {
