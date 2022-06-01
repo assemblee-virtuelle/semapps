ebb11bfdc74c5019b5d998c355a3e2fb878323f9
srosset81 <srosset81@gmail.com>
2022-06-01 16:21:45 +0200

    Discourse importer + doc

--- src/middleware/packages/importer/index.js
+++ src/middleware/packages/importer/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   ImporterMixin: require('./mixins/importer'),
+  DiscourseImporterMixin: require('./mixins/discourse'),
   DrupalImporterMixin: require('./mixins/drupal'),
   GoGoCartoImporterMixin: require('./mixins/gogocarto'),
   MobilizonImporterMixin: require('./mixins/mobilizon'),
