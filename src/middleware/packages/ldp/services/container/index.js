2e2ad0204eb2585df6c439029f6b6249896aba14
Niko <info@parlepeuple.fr>
2022-06-03 22:12:50 +0300

    ldp.container.isEmpty action

--- src/middleware/packages/ldp/services/container/index.js
+++ src/middleware/packages/ldp/services/container/index.js
@@ -3,6 +3,7 @@ const clearAction = require('./actions/clear');
 const createAction = require('./actions/create');
 const detachAction = require('./actions/detach');
 const existAction = require('./actions/exist');
+const isEmptyAction = require('./actions/isEmpty');
 const getAction = require('./actions/get');
 const getAllAction = require('./actions/getAll');
 const getUrisAction = require('./actions/getUris');
@@ -30,6 +31,7 @@ module.exports = {
     getUris: getUrisAction,
     includes: includesAction,
     post: postAction.action,
+    isEmpty: isEmptyAction,
     // Actions accessible through the API
     api_get: getAction.api,
     api_post: postAction.api,
