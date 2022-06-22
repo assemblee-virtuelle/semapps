fd475c9a6babbb077ade3e542a9ed24dc724c901
fluidlog <fluidlog@users.noreply.github.com>
2022-06-22 15:45:31 +0000

    Automatic prettier

--- src/frontend/packages/field-components/src/MultiUrlField.js
+++ src/frontend/packages/field-components/src/MultiUrlField.js
@@ -23,28 +23,26 @@ const useStyles = makeStyles(theme => ({
 const MultiUrlField = ({ source, ...rest }) => {
   const record = useRecordContext();
   const classes = useStyles();
-  const urlArray = record[source] ? Array.isArray(record[source]) ? record[source] : [record[source]] : [];
-  return (
-    urlArray.map(url => {
-      if( !url.startsWith('http') ) url = 'https://' + url;
-      const parsedUrl = new URL(url);
-      if( !parsedUrl ) return null;
-      const parseUrlString = parsedUrl.toString();
-      const chip = { label: parseUrlString, icon: <LanguageIcon />, color: '#ea', contrastText: 'black' };
-      return(
-        <a href={url} target="_blank" rel="noopener noreferrer" className={classes.link}>
-          <Chip
-            icon={React.cloneElement(chip.icon, { style: { color: chip.contrastText } })}
-            size="small"
-            label={chip.label}
-            classes={{ root: classes.chip, label: classes.label }}
-            style={{ color: chip.contrastText, backgroundColor: chip.color }}
-            {...rest}
-          />
-        </a>
-      )
-    })
-  );
+  const urlArray = record[source] ? (Array.isArray(record[source]) ? record[source] : [record[source]]) : [];
+  return urlArray.map(url => {
+    if (!url.startsWith('http')) url = 'https://' + url;
+    const parsedUrl = new URL(url);
+    if (!parsedUrl) return null;
+    const parseUrlString = parsedUrl.toString();
+    const chip = { label: parseUrlString, icon: <LanguageIcon />, color: '#ea', contrastText: 'black' };
+    return (
+      <a href={url} target="_blank" rel="noopener noreferrer" className={classes.link}>
+        <Chip
+          icon={React.cloneElement(chip.icon, { style: { color: chip.contrastText } })}
+          size="small"
+          label={chip.label}
+          classes={{ root: classes.chip, label: classes.label }}
+          style={{ color: chip.contrastText, backgroundColor: chip.color }}
+          {...rest}
+        />
+      </a>
+    );
+  });
 };
 
 MultiUrlField.defaultProps = {
--- src/frontend/packages/input-components/src/MultiLinesInput.js
+++ src/frontend/packages/input-components/src/MultiLinesInput.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { TextInput } from 'react-admin';
 
-const MultiLinesInput = (props) => (
+const MultiLinesInput = props => (
   <TextInput
     multiline
     minRows={2}
-    format={value => value ? Array.isArray(value) ? value.join('\n') : value : ''}
+    format={value => (value ? (Array.isArray(value) ? value.join('\n') : value) : '')}
     parse={value => value.split(/\r?\n/)}
     {...props}
   />
