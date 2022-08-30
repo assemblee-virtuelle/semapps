9fee31b208b714a008a11eafbcc65e8467f25987
Vincentfarcy <v.farcy72@gmail.com>
2022-08-30 22:11:33 +0200

    MarkdownInput validation managemment

--- src/frontend/packages/markdown-components/src/MarkdownInput.js
+++ src/frontend/packages/markdown-components/src/MarkdownInput.js
@@ -1,35 +1,53 @@
 import React, { useState } from 'react';
 import ReactMde from 'react-mde';
 import Markdown from 'markdown-to-jsx';
-import { useInput, InputHelperText } from 'react-admin';
-import { FormControl, FormHelperText } from '@material-ui/core';
+import { useInput, InputHelperText, Labeled, required } from 'react-admin';
+import { FormControl, FormHelperText, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  validationRequired: {
+    '& .MuiFormLabel-root': {
+      color: '#f44336',
+    },
+    '& .mde-text': {
+      outline: '-webkit-focus-ring-color auto 1px',
+      outlineOffset: 0,
+      outlineColor: '#f44336',
+      outlineStyle: 'auto',
+      outlineWidth: 1
+    }
+  },
+}));
 
 const MarkdownInput = props => {
+  const classes = useStyles();
+  const { validate } = props;
   const [tab, setTab] = useState('write');
   const {
-    input: { value, onChange },
+    input: { value, onChange, onBlur },
     meta: { error, touched }
   } = useInput(props);
 
   return (
-    <FormControl fullWidth className="ra-input-mde">
-      <ReactMde
-        value={value}
-        onChange={value => onChange(value)}
-        onTabChange={tab => setTab(tab)}
-        generateMarkdownPreview={async markdown => <Markdown>{markdown}</Markdown>}
-        selectedTab={tab}
-        {...props}
-      />
+    <FormControl fullWidth className={`ra-input-mde ${((error==='ra.validation.required') ? classes.validationRequired : '')}`} >
+      <Labeled {...props} isRequired={validate.toString()!==required.toString()} >
+        <ReactMde
+          value={value}
+          onChange={value => onChange(value)}
+          onTabChange={tab => setTab(tab)}
+          generateMarkdownPreview={async markdown => <Markdown>{markdown}</Markdown>}
+          selectedTab={tab}
+          childProps={{
+            textArea: {onBlur: ()=>onBlur()} 
+          }}
+          {...props}
+        />
+      </Labeled>
       <FormHelperText error={!!error} margin="dense" variant="outlined">
-        <InputHelperText error={error} helperText={props.helperText} touched={touched} />
+        <InputHelperText error={error} helperText={props.helperText} touched={error||touched} />
       </FormHelperText>
     </FormControl>
   );
-};
-
-MarkdownInput.defaultProps = {
-  addLabel: true
 };
 
 export default MarkdownInput;
