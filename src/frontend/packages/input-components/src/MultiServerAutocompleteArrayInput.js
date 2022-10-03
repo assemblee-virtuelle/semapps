c92c426734162209af327c7629c0f483baf17e41
Yannick DUTHE <yannickduthe@hotmail.com>
2022-10-03 17:45:14 +0200

    Allow optionText function AND Create props

--- src/frontend/packages/input-components/src/MultiServerAutocompleteArrayInput.js
+++ src/frontend/packages/input-components/src/MultiServerAutocompleteArrayInput.js
@@ -1,18 +1,21 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { AutocompleteArrayInput } from 'react-admin';
 import { useDataServers } from '@semapps/semantic-data-provider';
 import OptionRenderer from './OptionRenderer';
 
 const MultiServerAutocompleteArrayInput = ({ optionText, ...rest }) => {
   const dataServers = useDataServers();
+  const [suggestion, setSuggestion] = useState("");
   const matchSuggestion = useCallback(
-    (filterValue, choice) => choice[optionText].toLowerCase().match(filterValue.toLowerCase()),
-    [optionText]
+    (filterValue, choice) => {
+      setSuggestion(filterValue)
+      return ( choice[optionText].toLowerCase().match(filterValue.toLowerCase()) )
+    }, [optionText]
   );
   return (
     <AutocompleteArrayInput
       matchSuggestion={matchSuggestion}
-      optionText={<OptionRenderer optionText={optionText} dataServers={dataServers} />}
+      optionText={<OptionRenderer optionText={optionText} dataServers={dataServers} suggestion={suggestion} />}
       {...rest}
     />
   );
--- src/frontend/packages/input-components/src/OptionRenderer.js
+++ src/frontend/packages/input-components/src/OptionRenderer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const OptionRenderer = ({ record, optionText, dataServers }) => {
+const OptionRenderer = ({ record, optionText, dataServers, suggestion }) => {
   const server = dataServers && Object.values(dataServers).find(server => record.id.startsWith(server.baseUrl));
+  if (!record || !server) return "Créer "+suggestion ;
   return (
     <span>
       {record[optionText]}
