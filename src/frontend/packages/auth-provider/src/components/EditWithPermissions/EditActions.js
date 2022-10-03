84519a7f30e941b64116048e217f73293e60946a
srosset81 <srosset81@gmail.com>
2022-10-03 18:01:57 +0200

    Improve ShowActions and EditActions

--- src/frontend/packages/auth-provider/src/components/EditWithPermissions/EditActions.js
+++ src/frontend/packages/auth-provider/src/components/EditWithPermissions/EditActions.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { ShowButton, ListButton } from 'react-admin';
-import { TopToolbar } from '@semapps/archipelago-layout';
+import { usePermissionsOptimized, ShowButton, ListButton, TopToolbar } from 'react-admin';
 import PermissionsButton from '../PermissionsButton/PermissionsButton';
+import { rightsToControl } from "../../constants";
 
-const EditActions = ({ basePath, className, data, hasList, hasShow, hasControl, ...otherProps }) => (
-  <TopToolbar className={className} {...otherProps}>
-    {hasList && <ListButton basePath={basePath} record={data} />}
-    {hasShow && <ShowButton basePath={basePath} record={data} />}
-    {data && hasControl && <PermissionsButton basePath={basePath} record={data} />}
-  </TopToolbar>
-);
+const EditActions = ({ basePath, record, hasList, hasShow }) => {
+  const { permissions } = usePermissionsOptimized(record.id);
+  return (
+    <TopToolbar>
+      {hasList && <ListButton basePath={basePath} record={record} />}
+      {hasShow && <ShowButton basePath={basePath} record={record} />}
+      {!!permissions && permissions.some(p => rightsToControl.includes(p['acl:mode'])) && <PermissionsButton basePath={basePath} record={record} />}
+    </TopToolbar>
+  );
+}
 
 export default EditActions;
--- src/frontend/packages/auth-provider/src/components/ShowWithPermissions/ShowActions.js
+++ src/frontend/packages/auth-provider/src/components/ShowWithPermissions/ShowActions.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { EditButton, ListButton } from 'react-admin';
-import { TopToolbar } from '@semapps/archipelago-layout';
+import { EditButton, ListButton, TopToolbar, usePermissionsOptimized } from 'react-admin';
 import PermissionsButton from '../PermissionsButton/PermissionsButton';
+import { rightsToControl, rightsToEdit } from "../../constants";
 
-const ShowActions = ({ basePath, className, data, hasList, hasEdit, hasControl, ...otherProps }) => (
-  <TopToolbar className={className} {...otherProps}>
-    {hasList && <ListButton basePath={basePath} record={data} />}
-    {hasEdit && <EditButton basePath={basePath} record={data} />}
-    {data && hasControl && <PermissionsButton basePath={basePath} record={data} />}
-  </TopToolbar>
-);
+const ShowActions = ({ basePath, record, hasList, hasEdit }) => {
+  const { permissions } = usePermissionsOptimized(record.id);
+  return (
+    <TopToolbar>
+      {hasList && <ListButton basePath={basePath} record={record} />}
+      {hasEdit && permissions && permissions.some(p => rightsToEdit.includes(p['acl:mode'])) && <EditButton basePath={basePath} record={record} />}
+      {permissions && permissions.some(p => rightsToControl.includes(p['acl:mode'])) && <PermissionsButton basePath={basePath} record={record} />}
+    </TopToolbar>
+  );
+};
 
 export default ShowActions;
