d7427125735164fb3e24fb44a6d959df8d259071
srosset81 <srosset81@gmail.com>
2022-10-03 10:33:43 +0200

    Init move

--- src/frontend/packages/field-components/src/index.js
+++ src/frontend/packages/field-components/src/index.js
@@ -1,3 +1,5 @@
 export { default as AvatarWithLabelField } from './AvatarWithLabelField';
+export { default as ReferenceArrayField } from './ReferenceArrayField';
+export { default as ReferenceField } from './ReferenceField';
 export { default as QuickAppendReferenceArrayField } from './QuickAppendReferenceArrayField/QuickAppendReferenceArrayField';
 export { default as MultiUrlField } from './MultiUrlField';
