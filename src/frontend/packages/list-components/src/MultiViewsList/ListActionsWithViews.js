498351b472f0b0aee4ea0eabec2a95081d113ba4
srosset81 <srosset81@gmail.com>
2022-10-04 16:01:20 +0200

    Refactor MultiViewsList to use context

--- src/frontend/packages/list-components/src/MultiViewsList/ListActionsWithViews.js
+++ src/frontend/packages/list-components/src/MultiViewsList/ListActionsWithViews.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Button, CreateButton, ExportButton, useResourceDefinition, Link, TopToolbar } from 'react-admin';
-import { useLocation } from 'react-router';
+import { CreateButton, ExportButton, useResourceDefinition, TopToolbar } from 'react-admin';
 import { useMediaQuery } from '@material-ui/core';
+import ViewsButtons from './ViewsButtons';
 
 const ListActionsWithViews = ({
   bulkActions,
@@ -16,35 +16,13 @@ const ListActionsWithViews = ({
   selectedIds,
   showFilter,
   total,
-  views,
-  currentView,
-  setView,
   ...rest
 }) => {
   const xs = useMediaQuery(theme => theme.breakpoints.down('xs'));
   const resourceDefinition = useResourceDefinition(rest);
-  const query = new URLSearchParams(useLocation().search);
   return (
-    <TopToolbar currentView={currentView}>
-      {views &&
-        Object.entries(views)
-          .filter(([key]) => key !== currentView)
-          .map(([key, view]) => {
-            query.set('view', key);
-            query.set('page', 1);
-            query.set('perPage', view.perPage);
-            if (view.sort) {
-              query.set('sort', view.sort.field);
-              query.set('order', view.sort.order);
-            }
-            return (
-              <Link key={key} to={'?' + query.toString()}>
-                <Button onClick={() => setView(key)} label={view.label}>
-                  {React.createElement(view.icon)}
-                </Button>
-              </Link>
-            );
-          })}
+    <TopToolbar>
+      <ViewsButtons />
       {filters &&
         React.cloneElement(filters, {
           resource,
