c46d61e4338abee4c701a5a128f29870e768b2e0
srosset81 <srosset81@gmail.com>
2022-10-05 10:00:03 +0200

    Remove console.log

--- src/frontend/packages/semantic-data-provider/src/dataProvider/methods/getMany.js
+++ src/frontend/packages/semantic-data-provider/src/dataProvider/methods/getMany.js
@@ -2,17 +2,12 @@ import getOne from './getOne';
 
 const getManyMethod = config => async (resourceId, params) => {
   const { returnFailedResources } = config;
-  console.log('returnFailedResources', returnFailedResources, params);
 
   let returnData = await Promise.all(
     params.ids.map(id =>
       getOne(config)(resourceId, { id: typeof id === 'object' ? id['@id'] : id })
-        .then(({ data }) => {
-          console.log('data', data);
-          return data
-        })
-        .catch((e) => {
-          console.log('catch', e)
+        .then(({ data }) => data)
+        .catch(() => {
           // Catch if one resource fails to load
           // Otherwise no references will be show if only one is missing
           // See https://github.com/marmelab/react-admin/issues/5190
