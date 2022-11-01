ae207d2ba3795e50a60d365033ebe89a26606fdf
srosset81 <srosset81@gmail.com>
2022-11-01 17:03:23 +0100

    First try

--- src/frontend/packages/activitypub-components/src/hooks/useInbox.js
+++ src/frontend/packages/activitypub-components/src/hooks/useInbox.js
@@ -1,6 +1,12 @@
 import { useCallback, useMemo } from 'react';
 import { useGetIdentity, fetchUtils } from 'react-admin';
+import DataFactory from '@rdfjs/data-model';
 import { buildBlankNodesQuery } from '@semapps/semantic-data-provider';
+const { namedNode, triple, variable } = DataFactory;
+const SparqlGenerator = require('sparqljs').Generator;
+const generator = new SparqlGenerator({
+  /* prefixes, baseIRI, factory, sparqlStar */
+});
 
 const useInbox = () => {
   const { identity } = useGetIdentity();
@@ -19,37 +25,60 @@ const useInbox = () => {
 
   const fetch = useCallback(
     async ({ filters }) => {
-      console.log('inboxUrl', inboxUrl, sparqlEndpoint);
-
       if (!sparqlEndpoint || !inboxUrl) return;
 
-      const token = localStorage.getItem('token');
-      const blankNodesQuery = buildBlankNodesQuery(['as:object']);
+      const baseTriples = [
+        triple(namedNode(inboxUrl), namedNode('https://www.w3.org/ns/activitystreams#items'), variable('s1')),
+        triple(variable('s1'), variable('p1'), variable('o1'))
+      ];
 
-      let filtersWhereQuery = '';
-      if (filters) {
-        Object.keys(filters).forEach(predicate => {
-          if (filters[predicate]) {
-            const object = filters[predicate].startsWith('http') ? `<${filters[predicate]}>` : filters[predicate];
-            filtersWhereQuery += `?s1 ${predicate} ${object} .`;
-          }
-        });
+      const baseQuery = {
+        construct: baseTriples,
+        where: [
+          ...baseTriples
+        ],
       }
 
-      const query = `
-        PREFIX as: <https://www.w3.org/ns/activitystreams#>
-        CONSTRUCT {
-          ?s1 ?p1 ?o1 .
-          ${blankNodesQuery.construct}
+      const token = localStorage.getItem('token');
+      const asOntology = { prefix: "as", url: "https://www.w3.org/ns/activitystreams#" };
+      const blankNodesQuery = buildBlankNodesQuery(['as:object'], baseQuery, [asOntology]);
+
+      const sparqlJsParams = {
+        type: 'query',
+        queryType: 'CONSTRUCT',
+        template: blankNodesQuery.construct,
+        where: blankNodesQuery.where,
+        prefixes: {
+          as: 'https://www.w3.org/ns/activitystreams#'
         }
-        WHERE {
-          <${inboxUrl}> as:items ?s1 .
-          ?s1 ?p1 ?o1 .
-          FILTER( (isIRI(?s1)) ) .
-          ${filtersWhereQuery}
-          ${blankNodesQuery.where}
-        }
-      `;
+      };
+
+      // let filtersWhereQuery = '';
+      // if (filters) {
+      //   Object.keys(filters).forEach(predicate => {
+      //     if (filters[predicate]) {
+      //       const object = filters[predicate].startsWith('http') ? `<${filters[predicate]}>` : filters[predicate];
+      //       filtersWhereQuery += `?s1 ${predicate} ${object} .`;
+      //     }
+      //   });
+      // }
+      //
+      // const query = `
+      //   PREFIX as: <https://www.w3.org/ns/activitystreams#>
+      //   CONSTRUCT {
+      //     ?s1 ?p1 ?o1 .
+      //     ${blankNodesQuery.construct}
+      //   }
+      //   WHERE {
+      //     <${inboxUrl}> as:items ?s1 .
+      //     ?s1 ?p1 ?o1 .
+      //     FILTER( (isIRI(?s1)) ) .
+      //     ${filtersWhereQuery}
+      //     ${blankNodesQuery.where}
+      //   }
+      // `;
+
+      const query = generator.stringify(sparqlJsParams);
 
       const { json } = await fetchUtils.fetchJson(sparqlEndpoint, {
         method: 'POST',
