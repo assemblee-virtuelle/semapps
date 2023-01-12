20932ec2cc8ff7d78d0640eb1350840ce0ba44d4
Vincentfarcy <v.farcy72@gmail.com>
2023-01-12 13:46:18 +0100

    LogoutButton

--- src/frontend/packages/auth-provider/src/components/UserMenu.js
+++ src/frontend/packages/auth-provider/src/components/UserMenu.js
@@ -2,6 +2,7 @@ import React, { forwardRef } from 'react';
 import { UserMenu as RaUserMenu, MenuItemLink, useGetIdentity, useTranslate } from 'react-admin';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import EditIcon from '@mui/icons-material/Edit';
+import LogoutButton from './LogoutButton';
 
 const ViewProfileMenu = forwardRef(({ onClick, label, webId }, ref) => (
   <MenuItemLink
@@ -31,7 +32,7 @@ const SignupMenu = forwardRef(({ onClick, label }, ref) => (
   <MenuItemLink ref={ref} to="/login?signup=true" primaryText={label} onClick={onClick} />
 ));
 
-const UserMenu = ({ logout, ...otherProps }) => {
+const UserMenu = ({ logout=<LogoutButton/>, ...otherProps }) => {
   const { identity } = useGetIdentity();
   const translate = useTranslate();
   return (
