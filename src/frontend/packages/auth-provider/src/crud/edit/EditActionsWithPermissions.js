27afff21b3c96422b0c3ba1226ee953bade2ab5d
Vincentfarcy <v.farcy72@gmail.com>
2023-01-14 09:10:05 +0100

    get resource configuration flags with useResourceDefinition hook

--- src/frontend/packages/auth-provider/src/crud/edit/EditActionsWithPermissions.js
+++ src/frontend/packages/auth-provider/src/crud/edit/EditActionsWithPermissions.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { usePermissionsOptimized, ShowButton, ListButton, TopToolbar } from 'react-admin';
+import { usePermissionsOptimized, ShowButton, ListButton, TopToolbar, useResourceDefinition } from 'react-admin';
 import PermissionsButton from '../../components/PermissionsButton/PermissionsButton';
 import { rightsToControl } from '../../constants';
 
-const EditActionsWithPermissions = ({ basePath, record, hasList, hasShow }) => {
+const EditActionsWithPermissions = ({ record }) => {
+  const { hasList, hasShow } = useResourceDefinition();
   const { permissions } = usePermissionsOptimized(record?.id);
   return (
     <TopToolbar>
-      {hasList && <ListButton basePath={basePath} record={record} />}
-      {hasShow && <ShowButton basePath={basePath} record={record} />}
+      {hasList && <ListButton record={record} />}
+      {hasShow && <ShowButton record={record} />}
       {!!permissions && permissions.some(p => rightsToControl.includes(p['acl:mode'])) && (
-        <PermissionsButton basePath={basePath} record={record} />
+        <PermissionsButton record={record} />
       )}
     </TopToolbar>
   );
--- src/frontend/packages/auth-provider/src/crud/show/ShowActionsWithPermissions.js
+++ src/frontend/packages/auth-provider/src/crud/show/ShowActionsWithPermissions.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { EditButton, ListButton, TopToolbar, usePermissionsOptimized } from 'react-admin';
+import { EditButton, ListButton, TopToolbar, usePermissionsOptimized, useResourceDefinition } from 'react-admin';
 import PermissionsButton from '../../components/PermissionsButton/PermissionsButton';
 import { rightsToControl, rightsToEdit } from '../../constants';
 
-const ShowActionsWithPermissions = ({ basePath, record, hasList, hasEdit }) => {
+const ShowActionsWithPermissions = ({ record }) => {
+  const { hasList, hasEdit } = useResourceDefinition();
   const { permissions } = usePermissionsOptimized(record?.id);
   return (
     <TopToolbar>
-      {hasList && <ListButton basePath={basePath} record={record} />}
+      {hasList && <ListButton record={record} />}
       {hasEdit && permissions && permissions.some(p => rightsToEdit.includes(p['acl:mode'])) && (
-        <EditButton basePath={basePath} record={record} />
+        <EditButton record={record} />
       )}
       {permissions && permissions.some(p => rightsToControl.includes(p['acl:mode'])) && (
-        <PermissionsButton basePath={basePath} record={record} />
+        <PermissionsButton record={record} />
       )}
     </TopToolbar>
   );
