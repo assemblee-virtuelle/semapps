b0bb45d8b067ec7b7bd34280f57b9c7dc677730e
Vincentfarcy <v.farcy72@gmail.com>
2023-01-16 22:34:50 +0100

    ReferenceArrayInput upgrade

--- src/frontend/packages/input-components/src/ReferenceArrayInput.js
+++ src/frontend/packages/input-components/src/ReferenceArrayInput.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { ReferenceArrayInput as RaReferenceArrayInput, useResourceContext } from 'react-admin';
+import { ReferenceArrayInput as RaReferenceArrayInput, useResourceContext, useRecordContext } from 'react-admin';
 
 const ReferenceArrayInput = props => {
   const resource = useResourceContext({});
+  let record = useRecordContext();
+  if (record && record[props.source]) {
+    let value = record[props.source];
+    // if the linked field value is not an array, turns it into an array.
+    // Necessary as JSON-LD are sometimes arrays, sometimes not (when there is one value)
+    // and the ReferenceArrayInput component only accept arrays
+    if (!Array.isArray(value)) value = [value];
+    // If a format prop was defined, apply it to the array
+    if (props.format) value = props.format(value);
+    // If the values are objects with @id field, turn it to a simple string
+    value = value.map(v => (typeof v === 'object' ? v.id || v['@id'] : v));
+    record[props.source] = value;
+  }
   return (
     <RaReferenceArrayInput
       {...props}
       resource={resource}
-      format={value => {
-        // If there is no value, return immediately
-        if (!value) return value;
-        // if the linked field value is not an array, turns it into an array.
-        // Necessary as JSON-LD are sometimes arrays, sometimes not (when there is one value)
-        // and the ReferenceArrayInput component only accept arrays
-        if (!Array.isArray(value)) value = [value];
-        // If a format prop was defined, apply it to the array
-        if (props.format) value = props.format(value);
-        // If the values are objects with @id field, turn it to a simple string
-        return value.map(v => (typeof v === 'object' ? v.id || v['@id'] : v));
-      }}
+      record={record}
     />
   );
 };
