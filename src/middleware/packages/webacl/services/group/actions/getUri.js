ee5018a5d322f6e04062a029d873e9e91bdc1670
srosset81 <srosset81@gmail.com>
2023-04-17 19:45:11 +0200

    New webacl.group.getUri action

---
+++ src/middleware/packages/webacl/services/group/actions/getUri.js
@@ -0,0 +1,14 @@
+const urlJoin = require('url-join');
+
+module.exports = {
+  action: {
+    visibility: 'public',
+    params: {
+      groupSlug: { type: 'string', optional: false, trim: true },
+    },
+    async handler(ctx) {
+      let { groupSlug } = ctx.params;
+      return urlJoin(this.settings.baseUrl, '_groups', groupSlug);
+    }
+  }
+};
