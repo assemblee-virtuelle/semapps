77c95fefc627c02a0e3b41d211fd9d2b02a8a9ee
srosset81 <srosset81@gmail.com>
2023-09-26 10:24:32 +0200

    Packages build

--- src/frontend/packages/markdown-components/dist/index.cjs.js
+++ src/frontend/packages/markdown-components/dist/index.cjs.js
@@ -1,4 +1,4 @@
-var $lSzTu$reactjsxdevruntime = require("react/jsx-dev-runtime");
+var $lSzTu$reactjsxruntime = require("react/jsx-runtime");
 var $lSzTu$react = require("react");
 var $lSzTu$reactadmin = require("react-admin");
 var $lSzTu$markdowntojsx = require("markdown-to-jsx");
@@ -14,12 +14,9 @@ function $parcel$interopDefault(a) {
   return a && a.__esModule ? a.default : a;
 }
 
-$parcel$export(module.exports, "MarkdownField", () => $e77dbe0cc63a4759$exports.default);
-$parcel$export(module.exports, "MarkdownInput", () => $014faea72789ebc1$exports.default);
+$parcel$export(module.exports, "MarkdownField", () => $e77dbe0cc63a4759$export$2e2bcd8739ae039);
+$parcel$export(module.exports, "MarkdownInput", () => $014faea72789ebc1$export$2e2bcd8739ae039);
 $parcel$export(module.exports, "useLoadLinks", () => $111c9a4f9553dc9d$export$2e2bcd8739ae039);
-var $e77dbe0cc63a4759$exports = {};
-
-$parcel$export($e77dbe0cc63a4759$exports, "default", () => $e77dbe0cc63a4759$export$2e2bcd8739ae039);
 
 
 
@@ -28,17 +25,17 @@ $parcel$export($e77dbe0cc63a4759$exports, "default", () => $e77dbe0cc63a4759$exp
 const $e77dbe0cc63a4759$var$MarkdownField = ({ source: source, LabelComponent: LabelComponent, overrides: overrides = {}, ...rest })=>{
     const record = (0, $lSzTu$reactadmin.useRecordContext)();
     if (!record || !(0, ($parcel$interopDefault($lSzTu$lodashget)))(record, source)) return null;
-    return /*#__PURE__*/ (0, $lSzTu$reactjsxdevruntime.jsxDEV)((0, ($parcel$interopDefault($lSzTu$markdowntojsx))), {
+    return /*#__PURE__*/ (0, $lSzTu$reactjsxruntime.jsx)((0, ($parcel$interopDefault($lSzTu$markdowntojsx))), {
         options: {
             createElement (type, props, children) {
-                if (props.label) return /*#__PURE__*/ (0, $lSzTu$reactjsxdevruntime.jsxDEV)((0, $lSzTu$reactjsxdevruntime.Fragment), {
+                if (props.label) return /*#__PURE__*/ (0, $lSzTu$reactjsxruntime.jsxs)((0, $lSzTu$reactjsxruntime.Fragment), {
                     children: [
-                        /*#__PURE__*/ (0, $lSzTu$reactjsxdevruntime.jsxDEV)(LabelComponent, {
+                        /*#__PURE__*/ (0, $lSzTu$reactjsxruntime.jsx)(LabelComponent, {
                             children: props.label
-                        }, void 0, false, void 0, void 0),
+                        }),
                         /*#__PURE__*/ (0, ($parcel$interopDefault($lSzTu$react))).createElement(type, props, children)
                     ]
-                }, void 0, true);
+                });
                 return /*#__PURE__*/ (0, ($parcel$interopDefault($lSzTu$react))).createElement(type, props, children);
             },
             overrides: {
@@ -48,11 +45,7 @@ const $e77dbe0cc63a4759$var$MarkdownField = ({ source: source, LabelComponent: L
             ...rest
         },
         children: (0, ($parcel$interopDefault($lSzTu$lodashget)))(record, source)
-    }, void 0, false, {
-        fileName: "packages/markdown-components/src/MarkdownField.js",
-        lineNumber: 11,
-        columnNumber: 5
-    }, undefined);
+    });
 };
 $e77dbe0cc63a4759$var$MarkdownField.defaultProps = {
     LabelComponent: "h2"
@@ -60,9 +53,6 @@ $e77dbe0cc63a4759$var$MarkdownField.defaultProps = {
 var $e77dbe0cc63a4759$export$2e2bcd8739ae039 = $e77dbe0cc63a4759$var$MarkdownField;
 
 
-var $014faea72789ebc1$exports = {};
-
-$parcel$export($014faea72789ebc1$exports, "default", () => $014faea72789ebc1$export$2e2bcd8739ae039);
 
 
 
@@ -94,56 +84,36 @@ const $014faea72789ebc1$var$MarkdownInput = (props)=>{
     ]);
     const [tab, setTab] = (0, $lSzTu$react.useState)("write");
     const { field: { value: value, onChange: onChange }, fieldState: { isDirty: isDirty, invalid: invalid, error: error, isTouched: isTouched } } = (0, $lSzTu$reactadmin.useInput)(props);
-    return /*#__PURE__*/ (0, $lSzTu$reactjsxdevruntime.jsxDEV)($014faea72789ebc1$var$StyledFormControl, {
+    return /*#__PURE__*/ (0, $lSzTu$reactjsxruntime.jsxs)($014faea72789ebc1$var$StyledFormControl, {
         fullWidth: true,
         className: `ra-input-mde ${invalid ? "validationError" : ""}`,
         children: [
-            /*#__PURE__*/ (0, $lSzTu$reactjsxdevruntime.jsxDEV)((0, $lSzTu$reactadmin.Labeled), {
+            /*#__PURE__*/ (0, $lSzTu$reactjsxruntime.jsx)((0, $lSzTu$reactadmin.Labeled), {
                 ...props,
                 isRequired: isRequired,
-                children: /*#__PURE__*/ (0, $lSzTu$reactjsxdevruntime.jsxDEV)((0, ($parcel$interopDefault($lSzTu$reactmde))), {
+                children: /*#__PURE__*/ (0, $lSzTu$reactjsxruntime.jsx)((0, ($parcel$interopDefault($lSzTu$reactmde))), {
                     value: value,
                     onChange: (value)=>onChange(value),
                     onTabChange: (tab)=>setTab(tab),
-                    generateMarkdownPreview: async (markdown)=>/*#__PURE__*/ (0, $lSzTu$reactjsxdevruntime.jsxDEV)((0, ($parcel$interopDefault($lSzTu$markdowntojsx))), {
+                    generateMarkdownPreview: async (markdown)=>/*#__PURE__*/ (0, $lSzTu$reactjsxruntime.jsx)((0, ($parcel$interopDefault($lSzTu$markdowntojsx))), {
                             children: markdown
-                        }, void 0, false, void 0, void 0),
+                        }),
                     selectedTab: tab,
                     ...props
-                }, void 0, false, {
-                    fileName: "packages/markdown-components/src/MarkdownInput.js",
-                    lineNumber: 41,
-                    columnNumber: 9
-                }, undefined)
-            }, void 0, false, {
-                fileName: "packages/markdown-components/src/MarkdownInput.js",
-                lineNumber: 40,
-                columnNumber: 7
-            }, undefined),
-            /*#__PURE__*/ (0, $lSzTu$reactjsxdevruntime.jsxDEV)((0, $lSzTu$muimaterial.FormHelperText), {
+                })
+            }),
+            /*#__PURE__*/ (0, $lSzTu$reactjsxruntime.jsx)((0, $lSzTu$muimaterial.FormHelperText), {
                 error: isDirty && invalid,
                 margin: "dense",
                 variant: "outlined",
-                children: /*#__PURE__*/ (0, $lSzTu$reactjsxdevruntime.jsxDEV)((0, $lSzTu$reactadmin.InputHelperText), {
+                children: /*#__PURE__*/ (0, $lSzTu$reactjsxruntime.jsx)((0, $lSzTu$reactadmin.InputHelperText), {
                     error: isDirty && invalid && error,
                     helperText: props.helperText,
                     touched: error || isTouched
-                }, void 0, false, {
-                    fileName: "packages/markdown-components/src/MarkdownInput.js",
-                    lineNumber: 51,
-                    columnNumber: 9
-                }, undefined)
-            }, void 0, false, {
-                fileName: "packages/markdown-components/src/MarkdownInput.js",
-                lineNumber: 50,
-                columnNumber: 7
-            }, undefined)
+                })
+            })
         ]
-    }, void 0, true, {
-        fileName: "packages/markdown-components/src/MarkdownInput.js",
-        lineNumber: 39,
-        columnNumber: 5
-    }, undefined);
+    });
 };
 var $014faea72789ebc1$export$2e2bcd8739ae039 = $014faea72789ebc1$var$MarkdownInput;
 
--- src/frontend/packages/markdown-components/dist/index.es.js
+++ src/frontend/packages/markdown-components/dist/index.es.js
@@ -1,4 +1,4 @@
-import {jsxDEV as $lL6sH$jsxDEV, Fragment as $lL6sH$Fragment} from "react/jsx-dev-runtime";
+import {jsx as $lL6sH$jsx, jsxs as $lL6sH$jsxs, Fragment as $lL6sH$Fragment} from "react/jsx-runtime";
 import $lL6sH$react, {useMemo as $lL6sH$useMemo, useState as $lL6sH$useState} from "react";
 import {useRecordContext as $lL6sH$useRecordContext, required as $lL6sH$required, useInput as $lL6sH$useInput, Labeled as $lL6sH$Labeled, InputHelperText as $lL6sH$InputHelperText, useDataProvider as $lL6sH$useDataProvider, useTranslate as $lL6sH$useTranslate} from "react-admin";
 import $lL6sH$markdowntojsx from "markdown-to-jsx";
@@ -7,12 +7,6 @@ import $lL6sH$reactmde from "react-mde";
 import {FormControl as $lL6sH$FormControl, FormHelperText as $lL6sH$FormHelperText} from "@mui/material";
 import {styled as $lL6sH$styled} from "@mui/system";
 
-function $parcel$export(e, n, v, s) {
-  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
-}
-var $cf6ed0ef7f5d4af7$exports = {};
-
-$parcel$export($cf6ed0ef7f5d4af7$exports, "default", () => $cf6ed0ef7f5d4af7$export$2e2bcd8739ae039);
 
 
 
@@ -21,17 +15,17 @@ $parcel$export($cf6ed0ef7f5d4af7$exports, "default", () => $cf6ed0ef7f5d4af7$exp
 const $cf6ed0ef7f5d4af7$var$MarkdownField = ({ source: source, LabelComponent: LabelComponent, overrides: overrides = {}, ...rest })=>{
     const record = (0, $lL6sH$useRecordContext)();
     if (!record || !(0, $lL6sH$lodashget)(record, source)) return null;
-    return /*#__PURE__*/ (0, $lL6sH$jsxDEV)((0, $lL6sH$markdowntojsx), {
+    return /*#__PURE__*/ (0, $lL6sH$jsx)((0, $lL6sH$markdowntojsx), {
         options: {
             createElement (type, props, children) {
-                if (props.label) return /*#__PURE__*/ (0, $lL6sH$jsxDEV)((0, $lL6sH$Fragment), {
+                if (props.label) return /*#__PURE__*/ (0, $lL6sH$jsxs)((0, $lL6sH$Fragment), {
                     children: [
-                        /*#__PURE__*/ (0, $lL6sH$jsxDEV)(LabelComponent, {
+                        /*#__PURE__*/ (0, $lL6sH$jsx)(LabelComponent, {
                             children: props.label
-                        }, void 0, false, void 0, void 0),
+                        }),
                         /*#__PURE__*/ (0, $lL6sH$react).createElement(type, props, children)
                     ]
-                }, void 0, true);
+                });
                 return /*#__PURE__*/ (0, $lL6sH$react).createElement(type, props, children);
             },
             overrides: {
@@ -41,11 +35,7 @@ const $cf6ed0ef7f5d4af7$var$MarkdownField = ({ source: source, LabelComponent: L
             ...rest
         },
         children: (0, $lL6sH$lodashget)(record, source)
-    }, void 0, false, {
-        fileName: "packages/markdown-components/src/MarkdownField.js",
-        lineNumber: 11,
-        columnNumber: 5
-    }, undefined);
+    });
 };
 $cf6ed0ef7f5d4af7$var$MarkdownField.defaultProps = {
     LabelComponent: "h2"
@@ -53,9 +43,6 @@ $cf6ed0ef7f5d4af7$var$MarkdownField.defaultProps = {
 var $cf6ed0ef7f5d4af7$export$2e2bcd8739ae039 = $cf6ed0ef7f5d4af7$var$MarkdownField;
 
 
-var $ccc6163e3bb48ca3$exports = {};
-
-$parcel$export($ccc6163e3bb48ca3$exports, "default", () => $ccc6163e3bb48ca3$export$2e2bcd8739ae039);
 
 
 
@@ -87,56 +74,36 @@ const $ccc6163e3bb48ca3$var$MarkdownInput = (props)=>{
     ]);
     const [tab, setTab] = (0, $lL6sH$useState)("write");
     const { field: { value: value, onChange: onChange }, fieldState: { isDirty: isDirty, invalid: invalid, error: error, isTouched: isTouched } } = (0, $lL6sH$useInput)(props);
-    return /*#__PURE__*/ (0, $lL6sH$jsxDEV)($ccc6163e3bb48ca3$var$StyledFormControl, {
+    return /*#__PURE__*/ (0, $lL6sH$jsxs)($ccc6163e3bb48ca3$var$StyledFormControl, {
         fullWidth: true,
         className: `ra-input-mde ${invalid ? "validationError" : ""}`,
         children: [
-            /*#__PURE__*/ (0, $lL6sH$jsxDEV)((0, $lL6sH$Labeled), {
+            /*#__PURE__*/ (0, $lL6sH$jsx)((0, $lL6sH$Labeled), {
                 ...props,
                 isRequired: isRequired,
-                children: /*#__PURE__*/ (0, $lL6sH$jsxDEV)((0, $lL6sH$reactmde), {
+                children: /*#__PURE__*/ (0, $lL6sH$jsx)((0, $lL6sH$reactmde), {
                     value: value,
                     onChange: (value)=>onChange(value),
                     onTabChange: (tab)=>setTab(tab),
-                    generateMarkdownPreview: async (markdown)=>/*#__PURE__*/ (0, $lL6sH$jsxDEV)((0, $lL6sH$markdowntojsx), {
+                    generateMarkdownPreview: async (markdown)=>/*#__PURE__*/ (0, $lL6sH$jsx)((0, $lL6sH$markdowntojsx), {
                             children: markdown
-                        }, void 0, false, void 0, void 0),
+                        }),
                     selectedTab: tab,
                     ...props
-                }, void 0, false, {
-                    fileName: "packages/markdown-components/src/MarkdownInput.js",
-                    lineNumber: 41,
-                    columnNumber: 9
-                }, undefined)
-            }, void 0, false, {
-                fileName: "packages/markdown-components/src/MarkdownInput.js",
-                lineNumber: 40,
-                columnNumber: 7
-            }, undefined),
-            /*#__PURE__*/ (0, $lL6sH$jsxDEV)((0, $lL6sH$FormHelperText), {
+                })
+            }),
+            /*#__PURE__*/ (0, $lL6sH$jsx)((0, $lL6sH$FormHelperText), {
                 error: isDirty && invalid,
                 margin: "dense",
                 variant: "outlined",
-                children: /*#__PURE__*/ (0, $lL6sH$jsxDEV)((0, $lL6sH$InputHelperText), {
+                children: /*#__PURE__*/ (0, $lL6sH$jsx)((0, $lL6sH$InputHelperText), {
                     error: isDirty && invalid && error,
                     helperText: props.helperText,
                     touched: error || isTouched
-                }, void 0, false, {
-                    fileName: "packages/markdown-components/src/MarkdownInput.js",
-                    lineNumber: 51,
-                    columnNumber: 9
-                }, undefined)
-            }, void 0, false, {
-                fileName: "packages/markdown-components/src/MarkdownInput.js",
-                lineNumber: 50,
-                columnNumber: 7
-            }, undefined)
+                })
+            })
         ]
-    }, void 0, true, {
-        fileName: "packages/markdown-components/src/MarkdownInput.js",
-        lineNumber: 39,
-        columnNumber: 5
-    }, undefined);
+    });
 };
 var $ccc6163e3bb48ca3$export$2e2bcd8739ae039 = $ccc6163e3bb48ca3$var$MarkdownInput;
 
