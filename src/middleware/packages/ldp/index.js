bcab0f8bd1fbf278c881a99b06af87e9d3b5c6e6
bastien.siguier <bastien.siguier@gmail.com>
2023-10-03 17:09:48 +0200

    Fix DisassemblyMixin

--- src/middleware/packages/ldp/index.js
+++ src/middleware/packages/ldp/index.js
@@ -9,6 +9,7 @@ module.exports = {
   ControlledContainerMixin: require('./mixins/controlled-container'),
   ImageProcessorMixin: require('./mixins/image-processor'),
   DocumentTaggerMixin: require('./mixins/document-tagger'),
+  DisassemblyMixin: require('./mixins/disassembly'),
   LdpAdapter: require('./adapter'),
   getContainerRoute: require('./routes/getContainerRoute'),
   getResourcesRoute: require('./routes/getResourcesRoute'),
