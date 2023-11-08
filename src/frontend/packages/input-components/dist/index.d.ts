7ca9402d5603409332e8c50595602379df52853c
Laurin Weger <59744144+Laurin-W@users.noreply.github.com>
2023-11-08 13:04:56 +0100

    build frontend

--- src/frontend/packages/input-components/dist/index.d.ts
+++ src/frontend/packages/input-components/dist/index.d.ts
@@ -1,29 +1,20 @@
-import { FunctionComponent } from 'react';
-import { ImageInputProps } from 'react-admin';
+import { FunctionComponent } from "react";
+import { ImageInputProps } from "react-admin";
 type Props = ImageInputProps;
 export const ImageInput: FunctionComponent<Props>;
-export function MultiServerAutocompleteArrayInput({
-  optionText,
-  ...rest
-}: {
-  [x: string]: any;
-  optionText: any;
-}): import('react/jsx-runtime').JSX.Element;
-export function MultiServerAutocompleteInput({
-  optionText,
-  ...rest
-}: {
-  [x: string]: any;
-  optionText: any;
-}): import('react/jsx-runtime').JSX.Element;
-export function MultiLinesInput(props: any): import('react/jsx-runtime').JSX.Element;
-export function ReferenceArrayInput(props: any): import('react/jsx-runtime').JSX.Element | null;
-export function ReferenceInput({
-  children,
-  ...rest
-}: {
-  [x: string]: any;
-  children: any;
-}): import('react/jsx-runtime').JSX.Element;
+export function MultiServerAutocompleteArrayInput({ optionText, ...rest }: {
+    [x: string]: any;
+    optionText: any;
+}): import("react/jsx-runtime").JSX.Element;
+export function MultiServerAutocompleteInput({ optionText, ...rest }: {
+    [x: string]: any;
+    optionText: any;
+}): import("react/jsx-runtime").JSX.Element;
+export function MultiLinesInput(props: any): import("react/jsx-runtime").JSX.Element;
+export function ReferenceArrayInput(props: any): import("react/jsx-runtime").JSX.Element | null;
+export function ReferenceInput({ children, ...rest }: {
+    [x: string]: any;
+    children: any;
+}): import("react/jsx-runtime").JSX.Element;
 
 //# sourceMappingURL=index.d.ts.map
