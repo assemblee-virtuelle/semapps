d5bd2dca993a2eee9f08ba0be2c18e123a13acd8
srosset81 <srosset81@gmail.com>
2024-01-03 17:44:16 +0100

    Fix deploy

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'SemApps',
   tagline: 'A toolbox for semantic web applications',
   url: 'https://assemblee-virtuelle.github.io',
-  baseUrl: '/semapps',
+  baseUrl: '/semapps/',
   favicon: 'img/favicon.ico',
   organizationName: 'assemblee-virtuelle', // Usually your GitHub org/user name.
   projectName: 'semapps', // Usually your repo name.
