ff26f3e80ee3969a69ea54d9a7280147a48c7c06
Laurin Weger <59744144+Laurin-W@users.noreply.github.com>
2024-05-08 10:47:32 +0200

    rm AP dependency from webId

--- src/middleware/packages/webid/service.js
+++ src/middleware/packages/webid/service.js
@@ -2,16 +2,14 @@ const urlJoin = require('url-join');
 const { MIME_TYPES } = require('@semapps/mime-types');
 const { foaf, schema } = require('@semapps/ontologies');
 const { ControlledContainerMixin, ControlledContainerDereferenceMixin } = require('@semapps/ldp');
-const { FULL_ACTOR_TYPES } = require('@semapps/activitypub');
 
 /** @type {import('moleculer').ServiceSchema} */
 const WebIdService = {
   name: 'webid',
   settings: {
-    path: '/',
+    path: '/foaf/person',
     baseUrl: null,
-    acceptedTypes: Object.values(FULL_ACTOR_TYPES),
-
+    acceptedTypes: ['http://xmlns.com/foaf/0.1/Person'],
     defaultAccept: 'text/turtle',
     podProvider: false,
     podsContainer: false,
@@ -72,13 +70,15 @@ const WebIdService = {
         });
       } else {
         if (!this.settings.path) throw new Error('The path setting is required');
-        webId = await ctx.call('ldp.container.post', {
-          resource,
-          slug: nick,
-          containerUri: urlJoin(this.settings.baseUrl, this.settings.path),
-          contentType: MIME_TYPES.JSON,
-          webId: 'system'
-        });
+        webId = await this.actions.post(
+          {
+            resource,
+            slug: nick,
+            contentType: MIME_TYPES.JSON,
+            webId: 'system'
+          },
+          { parentCtx: ctx }
+        );
       }
 
       const newPerson = await ctx.call('ldp.resource.get', {
