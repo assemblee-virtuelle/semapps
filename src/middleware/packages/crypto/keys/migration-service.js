c6c26f31530bcdd602b03b009ea7195ffa3ec189
Laurin Weger <59744144+Laurin-W@users.noreply.github.com>
2024-05-24 17:34:52 +0200

    refactor migration service

--- src/middleware/packages/crypto/keys/migration-service.js
+++ src/middleware/packages/crypto/keys/migration-service.js
@@ -19,80 +19,48 @@ module.exports = {
         return;
       }
 
-      const users = fs
+      const accounts = await this.broker.call('auth.account.find');
+      const usernamesByKey = fs
         .readdirSync(this.settings.actorsKeyPairsDir)
         .filter(fn => fn.endsWith('.key'))
         .map(file => file.substring(0, file.length - 4));
 
-      if (users.length === 0) {
+      if (usernamesByKey.length === 0) {
         this.logger.warn("No keys to migrate, actorsKeyPairsDir doesn't contain any key files.");
         return;
       }
 
-      this.logger.info(`=== Migrating keys from filesystem to LDP for ${users.length} users ===`);
+      this.logger.info(`=== Migrating keys from filesystem to LDP ===`);
 
-      // For each actor...
-      for (const username of users) {
+      // Do the migration process.
+      try {
+        // Get keys with account info.
+        const accountsAndKeys = await this.getAccountsAndKeys();
+
+        // Delete public key material from webId (later replaced with references to the public key resources).
+        await this.deleteKeysFromWebId(accountsAndKeys);
+
+        // Create key resources in db and link to webIds.
+        await this.attachOrCreateToDb(accountsAndKeys);
+      } catch (err) {
+        // Error occurred during migration process. Try to revert to initial state.
         try {
-          const [user] = await ctx.call('auth.account.find', { query: { username } });
-
-          const { webId } = user;
-
-          // Get the user's RSA key
-          const { publicKey, privateKey } = await ctx.call('signature.keypair.get', { actorUri: webId, webId });
-          if (!publicKey || !privateKey) {
-            this.logger.warn(`Public or private key was not found for ${webId}, creating a new one...`);
-            await ctx.call('keys.createKeyForActor', { webId, keyType: KEY_TYPES.RSA, attachToWebId: true });
-            continue;
-          }
-
-          // Delete old public key blank node and data from the webId.
-          // Note: updating the triple store directly would usually require to delete the Redis cache for
-          // the webId, but since we are attaching the new public key just after this, it is not necessary.
-          await ctx.call('triplestore.update', {
-            // For podProvider context, the pod dataset is responsible, else default.
-            dataset: this.settings.podProvider ? username : undefined,
-            webId: 'system',
-            query: `
-            PREFIX sec: <https://w3id.org/security#>   
-            DELETE {
-              ?o  ?p1  ?o1 .
-              <${webId}> sec:publicKey  ?o .
-            }
-            WHERE {
-              <${webId}>  sec:publicKey  ?o .
-              ?o ?p1 ?o1 .
-            }`
-          });
-
-          // Add the key using the new keys service.
-          const keyResource = {
-            '@type': [KEY_TYPES.RSA, KEY_TYPES.VERIFICATION_METHOD],
-            publicKeyPem: publicKey,
-            privateKeyPem: privateKey,
-            owner: webId,
-            controller: webId
-          };
-          const resourceUri = await ctx.call('keys.container.post', {
-            resource: keyResource,
-            contentType: MIME_TYPES.JSON,
-            webId: webId
-          });
-          keyResource.id = resourceUri;
-
-          // Publish key.
-          await ctx.call(
-            'keys.attachPublicKeyToWebId',
-            { webId, keyObject: keyResource },
-            { meta: { skipMigrationCheck: true } }
-          );
-          this.logger.info('Migrated keys of user ', username);
-        } catch (error) {
-          this.logger.error(`Could not migrate user ${username}`, error);
+          await this.deleteKeysFromWebId(accounts);
+        } catch (_) {
+          // pass
         }
+
+        // Re-add possibly deleted publicKeys to webId document. This will do nothing, if keys are attached.
+        await Promise.all(
+          accounts.map(async ({ webId }) => ctx.call('signature.keypair.attachPublicKey', { actorUri: webId }))
+        );
+
+        this.logger.error('An error occurred during migration. Keys are not migrated:', err);
+        return;
       }
 
-      // Move fs keys to ./old folder
+      // Keys are stored in db now.
+      // Finally, move fs keys to ./old folder
       const keyFiles = fs
         .readdirSync(this.settings.actorsKeyPairsDir)
         .filter(fn => fn.endsWith('.key') || fn.endsWith('.key.pub'));
@@ -103,6 +71,25 @@ module.exports = {
           path.join(this.settings.actorsKeyPairsDir, 'old', keyFile)
         )
       );
+
+      // Stats about missing accounts and key-pairs.
+      const accountNames = accounts.map(acc => acc.username);
+      const keysWithoutRegisteredUser = usernamesByKey.filter(keyName => !accountNames.includes(keyName));
+      const usersWithoutKeys = accountNames.filter(accName => usernamesByKey.includes(accName));
+
+      if (keysWithoutRegisteredUser.length > 0) {
+        this.logger.warn(
+          `During the migration, the following keys were found that did not have a registered user associated:`,
+          keysWithoutRegisteredUser
+        );
+      }
+      if (usersWithoutKeys.length > 0) {
+        this.logger.warn(
+          `During the migration, the following accounts were found that did not have key pairs. New ones were created:`,
+          usersWithoutKeys
+        );
+      }
+
       this.logger.info('=== Keys migration completed ===');
       await ctx.emit('keys.migration.migrated');
     },
@@ -119,6 +106,81 @@ module.exports = {
         .find(fn => fn.endsWith('.key') || fn.endsWith('.key.pub'));
 
       return !anyKeyFile;
+    }
+  },
+  methods: {
+    async getAccountsAndKeys() {
+      /** @type {object[]} */
+      const accounts = await this.broker.call('auth.account.find');
+      return await Promise.all(
+        accounts.map(async account => {
+          // Get the user's RSA key
+          const { publicKey, privateKey } = await this.broker.call('signature.keypair.get', {
+            actorUri: account.webId,
+            webId: 'system'
+          });
+          // Key data is created later, if no full key material is available.
+          if (!publicKey || !privateKey) {
+            return account;
+          }
+          return { ...account, publicKey, privateKey };
+        })
+      );
+    },
+    async deleteKeysFromWebId(accounts) {
+      // Delete old public key blank node and data from the webId.
+      // Note: updating the triple store directly would usually require to delete the Redis cache for
+      // the webId, but since we are attaching the new public key in the next step, it is not necessary.
+      await Promise.all(
+        accounts.map(async ({ username, webId }) => {
+          await this.broker.call('triplestore.update', {
+            // For podProvider context, the pod dataset is responsible, else default.
+            dataset: this.settings.podProvider ? username : undefined,
+            webId: 'system',
+            query: `
+          PREFIX sec: <https://w3id.org/security#>   
+          DELETE {
+            <${webId}> sec:publicKey  ?o .
+            ?o  ?p1  ?o1 .
+          }
+          WHERE {
+            <${webId}>  sec:publicKey  ?o .
+            ?o ?p1 ?o1 .
+          }`
+          });
+        })
+      );
+    },
+    async attachOrCreateToDb(accountsAndKeys) {
+      // Add keys to container / create new keys where missing.
+      await Promise.all(
+        accountsAndKeys.map(async ({ webId, publicKey, privateKey }) => {
+          if (publicKey && privateKey) {
+            // Add the key using the new keys service.
+            const keyResource = {
+              '@type': [KEY_TYPES.RSA, KEY_TYPES.VERIFICATION_METHOD],
+              publicKeyPem: publicKey,
+              privateKeyPem: privateKey,
+              owner: webId,
+              controller: webId
+            };
+            const keyId = await this.broker.call('keys.container.post', {
+              resource: keyResource,
+              contentType: MIME_TYPES.JSON,
+              webId
+            });
+            keyResource.id = keyId;
+            // Publish key.
+            await this.broker.call(
+              'keys.attachPublicKeyToWebId',
+              { webId, keyObject: keyResource },
+              { meta: { skipMigrationCheck: true } }
+            );
+          } else {
+            await this.broker.call('keys.createKeyForActor', { webId, keyType: KEY_TYPES.RSA, attachToWebId: true });
+          }
+        })
+      );
     }
   }
 };
