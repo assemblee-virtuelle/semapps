70837a8f1629276e473537c19602d39457ade6b7
srosset81 <srosset81@gmail.com>
2024-08-22 17:51:00 +0200

    Remove console.log

--- src/middleware/packages/backup/utils/fsCopy.js
+++ src/middleware/packages/backup/utils/fsCopy.js
@@ -3,9 +3,7 @@ const { join: pathJoin } = require('path');
 
 const fsCopy = async (path, subDir, remoteServer) => {
   const destDir = pathJoin(remoteServer.path, subDir);
-  console.log(`Copying to ${destDir}`);
   await fs.copy(path, destDir);
-  console.log('Copy finished !');
 };
 
 module.exports = fsCopy;
