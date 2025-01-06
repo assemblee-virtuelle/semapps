dc0b2d1d7f1e10d0a321ef1c25d5c5bf03b001b7
srosset81 <srosset81@gmail.com>
2025-01-06 10:23:04 +0100

    Upload files on same server by default

--- src/frontend/packages/semantic-data-provider/src/dataProvider/methods/update.ts
+++ src/frontend/packages/semantic-data-provider/src/dataProvider/methods/update.ts
@@ -1,12 +1,15 @@
 import { RaRecord, UpdateParams } from 'react-admin';
 import { Configuration } from '../types';
 import handleFiles from '../utils/handleFiles';
+import getServerKeyFromUri from '../utils/getServerKeyFromUri';
 
 const updateMethod = (config: Configuration) => async (resourceId: string, params: UpdateParams<RaRecord>) => {
-  const { httpClient, jsonContext } = config;
+  const { httpClient, jsonContext, dataServers } = config;
+
+  const serverKey = getServerKeyFromUri(params.id, dataServers);
 
   // Upload files, if there are any
-  const { updatedRecord, filesToDelete } = await handleFiles.upload(params.data, config);
+  const { updatedRecord, filesToDelete } = await handleFiles.upload(params.data, config, serverKey);
   params.data = updatedRecord;
 
   await httpClient(`${params.id}`, {
