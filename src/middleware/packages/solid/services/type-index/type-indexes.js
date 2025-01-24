de83cbbe241a22d2e10fe93e7bdfe99f80e79005
srosset81 <srosset81@gmail.com>
2025-01-24 18:12:39 +0100

    Add PreferencesFileService and private type index

--- src/middleware/packages/solid/services/type-index/type-indexes.js
+++ src/middleware/packages/solid/services/type-index/type-indexes.js
@@ -11,22 +11,6 @@ module.exports = {
   settings: {
     acceptedTypes: ['solid:TypeIndex'],
     permissions: {},
-    newResourcesPermissions: webId => {
-      if (webId === 'anon' || webId === 'system')
-        throw new Error('Type indexes must be created by a registered webId.');
-
-      return {
-        anon: {
-          read: true
-        },
-        user: {
-          uri: webId,
-          read: true,
-          write: true,
-          control: true
-        }
-      };
-    },
     excludeFromMirror: true,
     activateTombstones: false,
     // DereferenceMixin settings
@@ -45,7 +29,7 @@ module.exports = {
     await this.broker.call('ontologies.register', apods);
   },
   actions: {
-    async createAndAttachToWebId(ctx) {
+    async createPublicIndex(ctx) {
       const { webId } = ctx.params;
 
       const indexUri = await this.actions.post(
@@ -54,6 +38,11 @@ module.exports = {
             type: ['solid:TypeIndex', 'solid:ListedDocument']
           },
           contentType: MIME_TYPES.JSON,
+          permissions: {
+            anon: {
+              read: true
+            }
+          },
           webId
         },
         { parentCtx: ctx }
@@ -67,7 +56,43 @@ module.exports = {
         webId
       });
     },
-    async findByWebId(ctx) {
+    async createPrivateIndex(ctx) {
+      const { webId } = ctx.params;
+
+      if (!(await this.preferencesFileAvailable()))
+        throw new Error(`The private type index requires the SolidPreferencesFile service`);
+
+      const preferencesUri = await ctx.call('solid-preferences-file.getResourceUri', { webId });
+      if (!preferencesUri) throw new Error(`No preferences file found for user ${webId}`);
+
+      const privateIndex = await this.actions.getPrivateIndex({ webId });
+      if (privateIndex) throw new Error(`A private index already exist for user ${webId}`);
+
+      const indexUri = await this.actions.post(
+        {
+          resource: {
+            type: ['solid:TypeIndex', 'solid:UnlistedDocument']
+          },
+          permissions: {},
+          contentType: MIME_TYPES.JSON,
+          webId
+        },
+        { parentCtx: ctx }
+      );
+
+      await ctx.call('solid-preferences-file.patch', {
+        resourceUri: preferencesUri,
+        triplesToAdd: [
+          triple(
+            namedNode(preferencesUri),
+            namedNode('http://www.w3.org/ns/solid/terms#privateTypeIndex'),
+            namedNode(indexUri)
+          )
+        ],
+        webId
+      });
+    },
+    async getPublicIndex(ctx) {
       const { webId } = ctx.params;
 
       const user = await ctx.call('ldp.resource.get', {
@@ -78,55 +103,53 @@ module.exports = {
 
       return user['solid:publicTypeIndex'];
     },
-    async migrate(ctx) {
-      const accounts = await ctx.call('auth.account.find');
-      for (const { webId } of accounts) {
-        this.logger.info(`Migrating ${webId}...`);
-        const podUrl = await ctx.call('solid-storage.getUrl', { webId });
-        await this.actions.createAndAttachToWebId({ webId }, { parentCtx: ctx });
+    async getPrivateIndex(ctx) {
+      const { webId } = ctx.params;
 
-        // Go through each registered container and persist them
-        const registeredContainers = await ctx.call('ldp.registry.list');
-        for (const container of Object.values(registeredContainers)) {
-          const containerUri = urlJoin(podUrl, container.path);
-          for (const type of arrayOf(container.acceptedTypes)) {
-            await ctx.call('type-registrations.register', { type, containerUri, webId });
-            if (container.description) {
-              await ctx.call('type-registrations.attachDescription', {
-                type,
-                webId,
-                ...container.description
-              });
-            }
-          }
-        }
-      }
+      if (!(await this.preferencesFileAvailable()))
+        throw new Error(`The private type index requires the SolidPreferencesFile service`);
+
+      const preferencesFileUri = await ctx.call('solid-preferences-file.get', { webId });
+
+      return preferencesFileUri['solid:privateTypeIndex'];
+    }
+  },
+  methods: {
+    async preferencesFileAvailable() {
+      const services = await this.broker.call('$node.services');
+      return services.some(s => s.name === 'solid-preferences-file');
     }
   },
   events: {
     async 'auth.registered'(ctx) {
       const { webId } = ctx.params;
+      const podUrl = await ctx.call('solid-storage.getUrl', { webId });
 
       // Wait until the /solid/type-index container has been created for the user
-      const containerUri = await this.actions.getContainerUri({ webId }, { parentCtx: ctx });
-      await this.actions.waitForContainerCreation({ containerUri }, { parentCtx: ctx });
+      const indexesContainerUri = await this.actions.getContainerUri({ webId }, { parentCtx: ctx });
+      await this.actions.waitForContainerCreation({ containerUri: indexesContainerUri }, { parentCtx: ctx });
 
       // Wait until the /solid/type-registration container has been created for the user
       const registrationsContainerUri = await ctx.call('type-registrations.getContainerUri', { webId });
       await ctx.call('type-registrations.waitForContainerCreation', { containerUri: registrationsContainerUri });
 
-      await this.actions.createAndAttachToWebId({ webId }, { parentCtx: ctx });
+      await this.actions.createPublicIndex({ webId }, { parentCtx: ctx });
+      await this.actions.createPrivateIndex({ webId }, { parentCtx: ctx });
 
       const registeredContainers = await ctx.call('ldp.registry.list');
 
       // Go through each registered container
       for (const container of Object.values(registeredContainers)) {
         if (container.podsContainer !== true) {
-          const podUrl = await ctx.call('solid-storage.getUrl', { webId });
           const containerUri = urlJoin(podUrl, container.path);
           for (const type of arrayOf(container.acceptedTypes)) {
             this.logger.info(`Registering ${containerUri} with type ${type}...`);
-            await ctx.call('type-registrations.register', { type, containerUri, webId });
+            await ctx.call('type-registrations.register', {
+              type,
+              containerUri,
+              webId,
+              private: container.description?.internal
+            });
             if (container.description) {
               await ctx.call('type-registrations.attachDescription', {
                 type,
