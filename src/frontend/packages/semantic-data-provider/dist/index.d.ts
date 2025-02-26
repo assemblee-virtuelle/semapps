e660b269edc8fc55c83cbf8532376467a89a13a8
srosset81 <srosset81@gmail.com>
2025-02-26 16:32:23 +0100

    New fetchTypeIndexes plugin

--- src/frontend/packages/semantic-data-provider/dist/index.d.ts
+++ src/frontend/packages/semantic-data-provider/dist/index.d.ts
@@ -166,6 +166,7 @@ export const configureUserStorage: () => Plugin;
  */
 export const fetchAppRegistration: () => Plugin;
 export const fetchDataRegistry: () => Plugin;
+export const fetchTypeIndexes: () => Plugin;
 export const fetchVoidEndpoints: () => Plugin;
 export const useCompactPredicate: (predicate: string, context?: ContextDefinition) => string | undefined;
 export const useDataModels: () => Record<string, import('index').DataModel> | undefined;
