05f9ff2a85ee87f25de8b8500c53af751db2fa51
mguihal <m.guihal@gmail.com>
2025-04-09 22:17:48 +0200

    fix: Remove uploaded file deletion in frontend packages

--- src/frontend/packages/semantic-data-provider/src/dataProvider/utils/handleFiles.ts
+++ src/frontend/packages/semantic-data-provider/src/dataProvider/utils/handleFiles.ts
@@ -3,8 +3,6 @@ import { Configuration } from '../types';
 import getUploadsContainerUri from './getUploadsContainerUri';
 
 const isFile = (o: any): o is { rawFile: File } => o?.rawFile && o.rawFile instanceof File;
-const isFileToDelete = (o: any): o is { fileToDelete: string } =>
-  o?.fileToDelete !== undefined && o?.fileToDelete !== null;
 
 export const uploadFile = async (rawFile: File, config: Configuration, serverKey?: string) => {
   const uploadsContainerUri = getUploadsContainerUri(config, serverKey);
@@ -25,22 +23,11 @@ export const uploadFile = async (rawFile: File, config: Configuration, serverKey
   return null;
 };
 
-const deleteFiles = async (filesToDelete: string[], config: Configuration) => {
-  return Promise.all(
-    filesToDelete.map(file =>
-      config.httpClient(file, {
-        method: 'DELETE'
-      })
-    )
-  );
-};
-
 /*
  * Look for raw files in the record data.
  * If there are any, upload them and replace the file by its URL.
  */
 const uploadAllFiles = async (record: Partial<RaRecord>, config: Configuration, serverKey?: string) => {
-  const filesToDelete: string[] = [];
   const updatedRecord = { ...record };
 
   for (const property of Object.keys(record)) {
@@ -49,33 +36,19 @@ const uploadAllFiles = async (record: Partial<RaRecord>, config: Configuration, 
       for (let i = 0; i < value.length; i++) {
         const itemValue = value[i];
         if (isFile(itemValue)) {
-          if (isFileToDelete(itemValue)) {
-            filesToDelete.push(itemValue.fileToDelete);
-          }
           updatedRecord[property][i] = await uploadFile(itemValue.rawFile, config, serverKey);
-        } else if (isFileToDelete(itemValue)) {
-          filesToDelete.push(itemValue.fileToDelete);
-          updatedRecord[property][i] = null;
         }
       }
     } else if (isFile(value)) {
-      if (isFileToDelete(value)) {
-        filesToDelete.push(value.fileToDelete);
-      }
       updatedRecord[property] = await uploadFile(value.rawFile, config, serverKey);
-    } else if (isFileToDelete(value)) {
-      filesToDelete.push(value.fileToDelete);
-      updatedRecord[property] = null;
     }
   }
 
   return {
-    updatedRecord,
-    filesToDelete
+    updatedRecord
   };
 };
 
 export default {
-  upload: uploadAllFiles,
-  delete: deleteFiles
+  upload: uploadAllFiles
 };
