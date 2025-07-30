fbe916c3b2d72a24475a09167913ca963ca159d3
Laurin Weger <59744144+Laurin-W@users.noreply.github.com>
2025-07-30 12:25:55 +0200

    add typecheck-hook for middleware

--- scripts/run-typecheck.sh
+++ scripts/run-typecheck.sh
@@ -4,14 +4,25 @@
 # Called by pre-push hook.
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
-cd "$ROOT_DIR"/src/frontend
 
 echo "=== Running typecheck for frontend ==="
 
+cd "$ROOT_DIR"/src/frontend
 yarn typecheck > /dev/null
 if [ $? -ne 0 ]; then
   echo "⛔ Typecheck failed! To see the errors, run \`yarn typecheck\` in the frontend directory."
   echo "   You can skip pre-push checks by setting \$SKIP_PREPUSH_CHECKS=true"
   exit 1
 fi
+
+echo "=== Running typecheck for middleware ==="
+
+cd "$ROOT_DIR"/src/middleware
+yarn typecheck > /dev/null
+if [ $? -ne 0 ]; then
+  echo "⛔ Typecheck failed! To see the errors, run \`yarn typecheck\` in the middleware directory."
+  echo "   You can skip pre-push checks by setting \$SKIP_PREPUSH_CHECKS=true"
+  exit 1
+fi
+
 echo "✅ Typecheck successful!"
