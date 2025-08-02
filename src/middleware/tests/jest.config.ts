d4b0a4365a4fea08e670b544ca9d70b3a9ac05c4
Laurin Weger <59744144+Laurin-W@users.noreply.github.com>
2025-08-02 11:26:56 +0200

    middleware: update yarn, jest config

--- src/middleware/tests/jest.config.ts
+++ src/middleware/tests/jest.config.ts
@@ -8,9 +8,6 @@ const config = {
   extensionsToTreatAsEsm: ['.ts'],
   transformIgnorePatterns: [],
 
-  // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
-  maxWorkers: '70%',
-
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ['./jest.setup.ts']
 };
