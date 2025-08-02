1d808b8b736d17d76703be4145bc1fb4784db1df
Laurin Weger <59744144+Laurin-W@users.noreply.github.com>
2025-08-02 09:56:38 +0200

    middleware tests: jest config

--- src/middleware/tests/config.ts
+++ src/middleware/tests/config.ts
@@ -1,6 +1,8 @@
 // Read all .env* files in the root folder and add them to process.env
 // See https://github.com/kerimdzhanov/dotenv-flow for more details
-require('dotenv-flow').config();
+import { config } from 'dotenv-flow';
+
+config();
 
 export const HOME_URL = process.env.SEMAPPS_HOME_URL;
 export const SPARQL_ENDPOINT = process.env.SEMAPPS_SPARQL_ENDPOINT;
@@ -10,3 +12,14 @@ export const JENA_USER = process.env.SEMAPPS_JENA_USER;
 export const JENA_PASSWORD = process.env.SEMAPPS_JENA_PASSWORD;
 export const ACTIVATE_CACHE = process.env.SEMAPPS_ACTIVATE_CACHE === 'true';
 export const FUSEKI_BASE = process.env.FUSEKI_BASE;
+
+export default {
+  HOME_URL: process.env.SEMAPPS_HOME_URL,
+  SPARQL_ENDPOINT: process.env.SEMAPPS_SPARQL_ENDPOINT,
+  MAIN_DATASET: process.env.SEMAPPS_MAIN_DATASET,
+  SETTINGS_DATASET: process.env.SEMAPPS_SETTINGS_DATASET,
+  JENA_USER: process.env.SEMAPPS_JENA_USER,
+  JENA_PASSWORD: process.env.SEMAPPS_JENA_PASSWORD,
+  ACTIVATE_CACHE: process.env.SEMAPPS_ACTIVATE_CACHE === 'true',
+  FUSEKI_BASE: process.env.FUSEKI_BASE
+};
