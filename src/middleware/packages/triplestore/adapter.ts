c77605d02e366bc4976ca2e59f9aafaeb69bb07a
Laurin Weger <59744144+Laurin-W@users.noreply.github.com>
2025-08-02 13:41:51 +0200

    middleware: fix incorrect ts-expect + import

--- src/middleware/packages/triplestore/adapter.ts
+++ src/middleware/packages/triplestore/adapter.ts
@@ -192,8 +192,10 @@ class TripleStoreAdapter {
               <${sanitizeSparqlUri(_id)}> ?p ?o .
             }
             INSERT {
-              // @ts-expect-error TS(2339): Property 'ontology' does not exist on type 'Triple... Remove this comment to see the full error message
-              <${sanitizeSparqlUri(_id)}> a <${this.ontology + this.type}> .
+              <${sanitizeSparqlUri(_id)}> a <${
+                // @ts-expect-error TS(2339): Property 'ontology' does not exist on type 'Triple... Remove this comment to see the full error message
+                this.ontology + this.type
+              }> .
               ${
                 newData
                   ? Object.keys(newData)
