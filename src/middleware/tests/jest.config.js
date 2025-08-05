4b9b4a93586dd440be1158e7915eb93fb794e0f1
Laurin Weger <59744144+Laurin-W@users.noreply.github.com>
2025-08-05 10:31:44 +0200

    middleware: tests: rename jest.config.ts to jest.config.js

--- src/middleware/tests/jest.config.js
+++ src/middleware/tests/jest.config.js
@@ -5,7 +5,7 @@
 
 const config = {
   // babel-jest
-  extensionsToTreatAsEsm: ['.ts'],
+  // extensionsToTreatAsEsm: ['.ts'],
   transformIgnorePatterns: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
